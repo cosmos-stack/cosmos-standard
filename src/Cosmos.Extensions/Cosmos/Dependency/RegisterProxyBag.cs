ecd587685c6246eb79c7e5e63d3f738179fcf300
alexinea <forerunner@outlook.com>
2019-03-16 00:19:09 +0800

    Add cosmos.dependency and dependency extensions

---
+++ src/Cosmos.Extensions/Cosmos/Dependency/RegisterProxyBag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cosmos.Dependency
+{
+    public class RegisterProxyBag
+    {
+        private readonly List<RegisterProxyDescriptor> _descriptors = new List<RegisterProxyDescriptor>();
+
+        public void Register(RegisterProxyDescriptor descriptor)
+        {
+            if (descriptor == null)
+                throw new ArgumentNullException(nameof(descriptor));
+
+            _descriptors.Add(descriptor);
+        }
+
+        public IReadOnlyList<RegisterProxyDescriptor> ExportDescriptors() => _descriptors.AsReadOnly();
+
+        public static RegisterProxyBag Create() => new RegisterProxyBag();
+    }
+}
---
+++ src/Cosmos.Extensions/Cosmos/Dependency/RegisterProxyDescriptor.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Cosmos.Dependency
+{
+    public class RegisterProxyDescriptor
+    {
+        public Type ServiceType { get; set; }
+
+        public Type ImplementationType { get; set; }
+
+        public Type ImplementationTypeSelf { get; set; }
+
+        public object InstanceOfImplementation { get; set; }
+
+        public Func<object> InstanceFuncForImplementation { get; set; }
+
+        public RegisterProxyType ProxyType { get; set; }
+
+        public RegisterProxyLifetimeType LifetimeType { get; set; }
+
+        public static RegisterProxyDescriptor Create<TService, TImplementation>(RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                ServiceType = typeof(TService),
+                ImplementationType = typeof(TImplementation),
+                ProxyType = RegisterProxyType.TypeToType,
+                LifetimeType = lifetimeType
+            };
+        }
+
+        public static RegisterProxyDescriptor Create<TService, TImplementation>(TImplementation instance, RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                ServiceType = typeof(TService),
+                InstanceOfImplementation = instance,
+                ProxyType = RegisterProxyType.TypeToInstance,
+                LifetimeType = lifetimeType
+            };
+        }
+
+        public static RegisterProxyDescriptor Create<TService>(object instance, RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                ServiceType = typeof(TService),
+                InstanceOfImplementation = instance,
+                ProxyType = RegisterProxyType.TypeToInstance,
+                LifetimeType = lifetimeType
+            };
+        }
+
+        public static RegisterProxyDescriptor Create<TImplementationSelf>(RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                ImplementationTypeSelf = typeof(TImplementationSelf),
+                ProxyType = RegisterProxyType.TypeSelf,
+                LifetimeType = lifetimeType
+            };
+        }
+
+        public static RegisterProxyDescriptor Create(object instance, RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                InstanceOfImplementation = instance,
+                ProxyType = RegisterProxyType.InstanceSelf,
+                LifetimeType = lifetimeType
+            };
+        }
+
+        public static RegisterProxyDescriptor Create<TService, TImplementation>(Func<TImplementation> instanceFunc, RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                ServiceType = typeof(TService),
+                ImplementationType = typeof(TImplementation),
+                InstanceFuncForImplementation = () => instanceFunc(),
+                ProxyType = RegisterProxyType.TypeToInstanceFunc,
+                LifetimeType = lifetimeType
+            };
+        }
+
+        public static RegisterProxyDescriptor Create<TService>(Func<object> instanceFunc, RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                ServiceType = typeof(TService),
+                InstanceFuncForImplementation = instanceFunc,
+                ProxyType = RegisterProxyType.TypeToInstanceFunc,
+                LifetimeType = lifetimeType
+            };
+        }
+
+        public static RegisterProxyDescriptor Create<TImplementation>(Func<TImplementation> instanceFunc, RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                ImplementationTypeSelf = typeof(TImplementation),
+                InstanceFuncForImplementation = () => instanceFunc(),
+                ProxyType = RegisterProxyType.InstanceSelfFunc,
+                LifetimeType = lifetimeType
+            };
+        }
+
+        public static RegisterProxyDescriptor Create(Func<object> instanceFunc, RegisterProxyLifetimeType lifetimeType)
+        {
+            return new RegisterProxyDescriptor
+            {
+                InstanceFuncForImplementation = instanceFunc,
+                ProxyType = RegisterProxyType.InstanceSelfFunc,
+                LifetimeType = lifetimeType
+            };
+        }
+    }
+}
