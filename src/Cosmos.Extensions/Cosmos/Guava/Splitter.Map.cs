703164a78a09375ec4f8473868ba78b4e4c77e7c
alexinea <forerunner@outlook.com>
2019-05-21 11:24:48 +0800

    Refactor Cosmos.Guava.Splitter

---
+++ src/Cosmos.Extensions/Cosmos/Guava/Splitter.Map.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cosmos.Guava
+{
+    public partial class Splitter : IGuavaMapSplitter
+    {
+
+        #region TrimResults
+
+        IGuavaMapSplitter IGuavaMapSplitter.TrimResults()
+        {
+            Options.SetTrimResults(k => k.Trim(), v => v.Trim());
+            return this;
+        }
+
+        IGuavaMapSplitter IGuavaMapSplitter.TrimResults(Func<string, string> keyTrimFunc, Func<string, string> valueTrimFunc)
+        {
+            Options.SetTrimResults(keyTrimFunc, valueTrimFunc);
+            return this;
+        }
+
+        #endregion
+
+        #region Limit
+
+        IGuavaMapSplitter IGuavaMapSplitter.Limit(int limit)
+        {
+            Options.SetLimitLength(limit);
+            return this;
+        }
+
+        #endregion
+
+        #region Split - KeyValuePair
+
+        IEnumerable<KeyValuePair<string, T>> IGuavaMapSplitter.Split<T>(string originalString, IObjectSerializer serializer)
+            => InternalSplitToKeyValuePair(originalString, serializer.Deserialize<T>);
+
+        IEnumerable<KeyValuePair<string, T>> IGuavaMapSplitter.Split<T>(string originalString, ITypeConverter<string, T> converter)
+            => InternalSplitToKeyValuePair(originalString, converter.To);
+
+        IEnumerable<KeyValuePair<string, string>> IGuavaMapSplitter.Split(string originalString)
+            => InternalSplitToKeyValuePair(originalString, s => s);
+
+        IEnumerable<KeyValuePair<string, T>> IGuavaMapSplitter.Split<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper)
+            => InternalSplitToKeyValuePair(originalString, s => mapper.MapTo<TMiddle, T>(serializer.Deserialize<TMiddle>(s)));
+
+        public Dictionary<string, string> SplitToDictionary(string originalString)
+            => ((IGuavaMapSplitter)this).Split(originalString).ToDictionary(k => k.Key, v => v.Value);
+
+        public Dictionary<string, T> SplitToDictionary<T>(string originalString, IObjectSerializer serializer)
+            => ((IGuavaMapSplitter)this).Split<T>(originalString, serializer).ToDictionary(k => k.Key, v => v.Value);
+
+        public Dictionary<string, T> SplitToDictionary<T>(string originalString, ITypeConverter<string, T> converter)
+            => ((IGuavaMapSplitter)this).Split(originalString, converter).ToDictionary(k => k.Key, v => v.Value);
+
+        public Dictionary<string, T> SplitToDictionary<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper)
+            => ((IGuavaMapSplitter)this).Split<TMiddle, T>(originalString, serializer, mapper).ToDictionary(k => k.Key, v => v.Value);
+
+        private IEnumerable<KeyValuePair<string, TValue>> InternalSplitToKeyValuePair<TValue>(string originalString, Func<string, TValue> to)
+        {
+            if (string.IsNullOrWhiteSpace(originalString))
+                return Enumerable.Empty<KeyValuePair<string, TValue>>();
+
+            var result = new List<KeyValuePair<string, TValue>>();
+            var middle = ((IGuavaSplitter)this).Split(originalString);
+
+            foreach (var item in middle)
+            {
+                var (k, v) = SplitterUtils.SplitMap(Options, item);
+                result.Add(SplitterUtils.OptionalMap(Options, k, v, to));
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region Private class
+
+        private partial class SplitterOptions
+        {
+            #region WithKeyValueSeparator
+
+            public string MapSeparator { get; private set; }
+
+            public void SetMapSeparator(string separator)
+            {
+                MapSeparator = separator;
+            }
+
+            public void SetMapSeparator(char separator)
+            {
+                MapSeparator = $"{separator}";
+            }
+
+            #endregion
+
+            #region TrimResults
+
+            public bool MapTrimResultsFlag { get; private set; }
+
+            public Func<string, string> KeyTrimFunc { get; private set; }
+
+            public Func<string, string> ValueTrimFunc { get; private set; }
+
+            public void SetTrimResults(Func<string, string> keyFunc, Func<string, string> valueFunc)
+            {
+                MapTrimResultsFlag = true;
+                KeyTrimFunc = keyFunc ?? (k => k.Trim());
+                ValueTrimFunc = valueFunc ?? (v => v.Trim());
+            }
+
+            #endregion
+        }
+
+        private static partial class SplitterUtils
+        {
+            public static KeyValuePair<string, TValue> OptionalMap<TValue>(SplitterOptions options, string key, string value, Func<string, TValue> to)
+            {
+                return options.MapTrimResultsFlag
+                    ? new KeyValuePair<string, TValue>(options.KeyTrimFunc(key), to(options.ValueTrimFunc(value)))
+                    : new KeyValuePair<string, TValue>(key, to(value));
+            }
+
+            public static (string, string) SplitMap(SplitterOptions options, string middleString)
+            {
+                var t = middleString.Split(options.MapSeparator);
+                var key = t[0];
+                var value = t.Length > 1 ? t[1] : string.Empty;
+                return (key, value);
+            }
+
+        }
+
+        #endregion
+    }
+
+    public interface IGuavaMapSplitter
+    {
+        IGuavaMapSplitter TrimResults();
+        IGuavaMapSplitter TrimResults(Func<string, string> keyTrimFunc, Func<string, string> valueTrimFunc);
+        IGuavaMapSplitter Limit(int limit);
+        IEnumerable<KeyValuePair<string, string>> Split(string originalString);
+        IEnumerable<KeyValuePair<string, T>> Split<T>(string originalString, IObjectSerializer serializer);
+        IEnumerable<KeyValuePair<string, T>> Split<T>(string originalString, ITypeConverter<string, T> converter);
+        IEnumerable<KeyValuePair<string, T>> Split<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper);
+        Dictionary<string, string> SplitToDictionary(string originalString);
+        Dictionary<string, T> SplitToDictionary<T>(string originalString, IObjectSerializer serializer);
+        Dictionary<string, T> SplitToDictionary<T>(string originalString, ITypeConverter<string, T> converter);
+        Dictionary<string, T> SplitToDictionary<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper);
+    }
+}
--- src/Cosmos.Extensions/Cosmos/Guava/Splitter.cs
+++ src/Cosmos.Extensions/Cosmos/Guava/Splitter.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+
 // ReSharper disable InconsistentNaming
 
 namespace Cosmos.Guava
 {
-    public class Splitter : IGuavaLikeSplitter, IKeyValuePairSplitter
+    public partial class Splitter : IGuavaSplitter
     {
         private readonly bool _regexMode;
         private readonly string[] _on;
         private readonly string _pattern;
         private readonly Regex _regexPattern;
+
+        private SplitterOptions Options { get; set; } = new SplitterOptions();
 
         private Splitter(string[] on)
         {
@@ -39,71 +42,41 @@ namespace Cosmos.Guava
 
         #region OmitEmptyStrings
 
-        private bool _omitEmptyStrings { get; set; }
-
-        public IGuavaLikeSplitter OmitEmptyStrings()
+        public IGuavaSplitter OmitEmptyStrings()
         {
-            _omitEmptyStrings = true;
+            Options.SetOmitEmptyStrings();
             return this;
         }
-
-        private StringSplitOptions _getStringSplitOptions() => _omitEmptyStrings ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None;
 
         #endregion
 
         #region WithKeyValueSeparator
 
-        private string _kvpSeparator { get; set; }
-
-        public IKeyValuePairSplitter WithKeyValueSeparator(string separator)
+        public IGuavaMapSplitter WithKeyValueSeparator(string separator)
         {
-            _kvpSeparator = separator;
+            Options.SetMapSeparator(separator);
             return this;
         }
 
-        public IKeyValuePairSplitter WithKeyValueSeparator(char separator)
+        public IGuavaMapSplitter WithKeyValueSeparator(char separator)
         {
-            return WithKeyValueSeparator($"{separator}");
+            Options.SetMapSeparator(separator);
+            return this;
         }
 
         #endregion
 
         #region TrimResults
 
-        private bool _trimResults { get; set; }
-        private Func<string, string> _trimFunc { get; set; } = s => s.Trim();
-        private bool _kvpTrimResults { get; set; }
-        private Func<string, string> _keyTrimFunc { get; set; } = s => s.Trim();
-        private Func<string, string> _valueTrimFunc { get; set; } = s => s.Trim();
-
-        IGuavaLikeSplitter IGuavaLikeSplitter.TrimResults()
+        IGuavaSplitter IGuavaSplitter.TrimResults()
         {
-            _trimResults = true;
-            _trimFunc = s => s.Trim();
+            Options.SetTrimResults();
             return this;
         }
 
-        IGuavaLikeSplitter IGuavaLikeSplitter.TrimResults(Func<string, string> trimFunc)
+        IGuavaSplitter IGuavaSplitter.TrimResults(Func<string, string> trimFunc)
         {
-            _trimResults = true;
-            _trimFunc = trimFunc;
-            return this;
-        }
-
-        IKeyValuePairSplitter IKeyValuePairSplitter.TrimResults()
-        {
-            _kvpTrimResults = true;
-            _keyTrimFunc = s => s.Trim();
-            _valueTrimFunc = s => s.Trim();
-            return this;
-        }
-
-
-        IKeyValuePairSplitter IKeyValuePairSplitter.TrimResults(Func<string, string> keyTrimFunc, Func<string, string> valueTrimFunc)
-        {
-            _kvpTrimResults = true;
-            _keyTrimFunc = keyTrimFunc;
-            _valueTrimFunc = valueTrimFunc;
+            Options.SetTrimResults(trimFunc);
             return this;
         }
 
@@ -111,16 +84,9 @@ namespace Cosmos.Guava
 
         #region Limit
 
-        private int _limit { get; set; } = -1;
-        IGuavaLikeSplitter IGuavaLikeSplitter.Limit(int limit)
+        IGuavaSplitter IGuavaSplitter.Limit(int limit)
         {
-            _limit = limit;
-            return this;
-        }
-
-        IKeyValuePairSplitter IKeyValuePairSplitter.Limit(int limit)
-        {
-            _limit = limit;
+            Options.SetLimitLength(limit);
             return this;
         }
 
@@ -128,31 +94,31 @@ namespace Cosmos.Guava
 
         #region Split - List
 
-        private bool _doesUseInLimitedMode() => _limit >= 0 && (!_regexMode || (_regexMode && _regexPattern == null));
+        private bool _doesUseInLimitedMode() => Options.LimitLength >= 0 && (!_regexMode || (_regexMode && _regexPattern == null));
 
-        IEnumerable<string> IGuavaLikeSplitter.Split(string originalString)
-            => InternalSplitToEnumerable<string>(originalString, s => s);
+        IEnumerable<string> IGuavaSplitter.Split(string originalString)
+            => InternalSplitToEnumerable(originalString, s => s);
 
-        IEnumerable<T> IGuavaLikeSplitter.Split<T>(string originalString, IObjectSerializer serializer)
-            => InternalSplitToEnumerable<T>(originalString, serializer.Deserialize<T>);
+        IEnumerable<T> IGuavaSplitter.Split<T>(string originalString, IObjectSerializer serializer)
+            => InternalSplitToEnumerable(originalString, serializer.Deserialize<T>);
 
-        IEnumerable<T> IGuavaLikeSplitter.Split<T>(string originalString, ITypeConverter<string, T> converter)
-            => InternalSplitToEnumerable<T>(originalString, converter.To);
+        IEnumerable<T> IGuavaSplitter.Split<T>(string originalString, ITypeConverter<string, T> converter)
+            => InternalSplitToEnumerable(originalString, converter.To);
 
-        IEnumerable<T> IGuavaLikeSplitter.Split<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper)
-            => InternalSplitToEnumerable<T>(originalString, s => mapper.MapTo<TMiddle, T>(serializer.Deserialize<TMiddle>(s)));
-        
+        IEnumerable<T> IGuavaSplitter.Split<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper)
+            => InternalSplitToEnumerable(originalString, s => mapper.MapTo<TMiddle, T>(serializer.Deserialize<TMiddle>(s)));
+
         public List<string> SplitToList(string originalString)
-            => ((IGuavaLikeSplitter)this).Split(originalString).ToList();
+            => ((IGuavaSplitter)this).Split(originalString).ToList();
 
         public List<T> SplitToList<T>(string originalString, IObjectSerializer serializer)
-            => ((IGuavaLikeSplitter)this).Split<T>(originalString, serializer).ToList();
+            => ((IGuavaSplitter)this).Split<T>(originalString, serializer).ToList();
 
         public List<T> SplitToList<T>(string originalString, ITypeConverter<string, T> converter)
-            => ((IGuavaLikeSplitter)this).Split<T>(originalString, converter).ToList();
+            => ((IGuavaSplitter)this).Split(originalString, converter).ToList();
 
         public List<T> SplitToList<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper)
-            => ((IGuavaLikeSplitter)this).Split<TMiddle, T>(originalString, serializer, mapper).ToList();
+            => ((IGuavaSplitter)this).Split<TMiddle, T>(originalString, serializer, mapper).ToList();
 
         private IEnumerable<TValue> InternalSplitToEnumerable<TValue>(string originalString, Func<string, TValue> to)
         {
@@ -161,20 +127,16 @@ namespace Cosmos.Guava
 
             var result = new List<string>();
             var middle = _regexMode
-                ? _regexPattern == null
-                    ? Regex.Split(originalString, _pattern)
-                    : _limit > 0
-                        ? _regexPattern.Split(originalString, _limit)
-                        : _regexPattern.Split(originalString)
-                : originalString.Split(_on, _getStringSplitOptions());
+                ? SplitterUtils.SplitPatternList(Options, originalString, _pattern, _regexPattern)
+                : SplitterUtils.SplitList(Options, originalString, _on);
 
             if (_doesUseInLimitedMode())
             {
-                result.AddRange(_trimResults ? middle.Select(item => _trimFunc?.Invoke(item) ?? item.Trim()) : middle, _limit);
+                result.AddRange(SplitterUtils.OptionalRange(Options, middle), Options.LimitLength);
             }
             else
             {
-                result.AddRange(_trimResults ? middle.Select(item => _trimFunc?.Invoke(item) ?? item.Trim()) : middle);
+                result.AddRange(SplitterUtils.OptionalRange(Options, middle));
             }
 
             return result.Select(to);
@@ -182,58 +144,9 @@ namespace Cosmos.Guava
 
         #endregion
 
-        #region Split - KeyValuePair
-
-        IEnumerable<KeyValuePair<string, T>> IKeyValuePairSplitter.Split<T>(string originalString, IObjectSerializer serializer)
-            => InternalSplitToKeyValuePair<T>(originalString, serializer.Deserialize<T>);
-
-        IEnumerable<KeyValuePair<string, T>> IKeyValuePairSplitter.Split<T>(string originalString, ITypeConverter<string, T> converter)
-            => InternalSplitToKeyValuePair<T>(originalString, converter.To);
-
-        IEnumerable<KeyValuePair<string, string>> IKeyValuePairSplitter.Split(string originalString)
-            => InternalSplitToKeyValuePair<string>(originalString, s => s);
-
-        IEnumerable<KeyValuePair<string, T>> IKeyValuePairSplitter.Split<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper)
-            => InternalSplitToKeyValuePair<T>(originalString, s => mapper.MapTo<TMiddle, T>(serializer.Deserialize<TMiddle>(s)));
-
-        public Dictionary<string, string> SplitToDictionary(string originalString)
-            => ((IKeyValuePairSplitter)this).Split(originalString).ToDictionary(k => k.Key, v => v.Value);
-
-        public Dictionary<string, T> SplitToDictionary<T>(string originalString, IObjectSerializer serializer)
-            => ((IKeyValuePairSplitter)this).Split<T>(originalString, serializer).ToDictionary(k => k.Key, v => v.Value);
-
-        public Dictionary<string, T> SplitToDictionary<T>(string originalString, ITypeConverter<string, T> converter)
-            => ((IKeyValuePairSplitter)this).Split<T>(originalString, converter).ToDictionary(k => k.Key, v => v.Value);
-
-        public Dictionary<string, T> SplitToDictionary<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper)
-            => ((IKeyValuePairSplitter)this).Split<TMiddle, T>(originalString, serializer, mapper).ToDictionary(k => k.Key, v => v.Value);
-
-        private IEnumerable<KeyValuePair<string, TValue>> InternalSplitToKeyValuePair<TValue>(string originalString, Func<string, TValue> to)
-        {
-            if (string.IsNullOrWhiteSpace(originalString))
-                return Enumerable.Empty<KeyValuePair<string, TValue>>();
-            var result = new List<KeyValuePair<string, TValue>>();
-            var middle = ((IGuavaLikeSplitter)this).Split(originalString);
-            foreach (var item in middle)
-            {
-                var t = item.Split(_kvpSeparator);
-                var key = t[0];
-                var value = t.Length > 1 ? t[1] : string.Empty;
-                result.Add(
-                    _kvpTrimResults
-                        ? new KeyValuePair<string, TValue>(_keyTrimFunc?.Invoke(key) ?? key.Trim(), to(_valueTrimFunc?.Invoke(value) ?? value.Trim()))
-                        : new KeyValuePair<string, TValue>(key, to(value))
-                );
-            }
-
-            return result;
-        }
-
-        #endregion
-
         #region On
 
-        public static IGuavaLikeSplitter On(string on, params string[] on2)
+        public static IGuavaSplitter On(string on, params string[] on2)
         {
             var o = new string[(on2?.Length ?? 0) + 1];
             o[0] = on;
@@ -242,27 +155,105 @@ namespace Cosmos.Guava
             return new Splitter(o);
         }
 
-        public static IGuavaLikeSplitter On(Regex pattern)
+        public static IGuavaSplitter On(Regex pattern)
         {
             return new Splitter(pattern);
         }
 
-        public static IGuavaLikeSplitter OnPattern(string separatorPattern)
+        public static IGuavaSplitter OnPattern(string separatorPattern)
         {
             return new Splitter(separatorPattern);
         }
 
         #endregion
+
+        #region Private class
+
+        private partial class SplitterOptions
+        {
+
+            #region OmitEmptyStrings
+
+            private bool OmitEmptyStrings { get; set; }
+
+            public void SetOmitEmptyStrings()
+            {
+                OmitEmptyStrings = true;
+            }
+
+            public StringSplitOptions GetStringSplitOptions() => OmitEmptyStrings ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None;
+
+            #endregion
+
+            #region Limit
+
+            public int LimitLength { get; private set; } = -1;
+
+            public void SetLimitLength(int limit)
+            {
+                if (limit <= 0)
+                    LimitLength = -1;
+                else
+                    LimitLength = limit;
+            }
+
+            #endregion
+
+            #region TrimResults
+
+            public bool TrimResultsFlag { get; private set; }
+
+            public Func<string, string> TrimFunc { get; private set; }
+
+            public void SetTrimResults()
+            {
+                TrimResultsFlag = true;
+                TrimFunc = s => s.Trim();
+            }
+
+            public void SetTrimResults(Func<string, string> func)
+            {
+                TrimResultsFlag = true;
+                TrimFunc = func ?? (s => s.Trim());
+            }
+
+            #endregion
+
+        }
+
+        private static partial class SplitterUtils
+        {
+            public static IEnumerable<string> OptionalRange(SplitterOptions options, string[] middleStrings)
+            {
+                return options.TrimResultsFlag ? middleStrings.Select(options.TrimFunc) : middleStrings;
+            }
+
+            public static string[] SplitList(SplitterOptions options, string originalString, string[] on)
+            {
+                return originalString.Split(on, options.GetStringSplitOptions());
+            }
+
+            public static string[] SplitPatternList(SplitterOptions options, string originalString, string stringPattern, Regex regexPattern)
+            {
+                return regexPattern == null
+                    ? Regex.Split(originalString, stringPattern)
+                    : options.LimitLength > 0
+                        ? regexPattern.Split(originalString, options.LimitLength)
+                        : regexPattern.Split(originalString);
+            }
+        }
+
+        #endregion
     }
 
-    public interface IGuavaLikeSplitter
+    public interface IGuavaSplitter
     {
-        IGuavaLikeSplitter OmitEmptyStrings();
-        IGuavaLikeSplitter TrimResults();
-        IGuavaLikeSplitter TrimResults(Func<string, string> trimFunc);
-        IGuavaLikeSplitter Limit(int limit);
-        IKeyValuePairSplitter WithKeyValueSeparator(char separator);
-        IKeyValuePairSplitter WithKeyValueSeparator(string separator);
+        IGuavaSplitter OmitEmptyStrings();
+        IGuavaSplitter TrimResults();
+        IGuavaSplitter TrimResults(Func<string, string> trimFunc);
+        IGuavaSplitter Limit(int limit);
+        IGuavaMapSplitter WithKeyValueSeparator(char separator);
+        IGuavaMapSplitter WithKeyValueSeparator(string separator);
         IEnumerable<string> Split(string originalString);
         IEnumerable<T> Split<T>(string originalString, IObjectSerializer serializer);
         IEnumerable<T> Split<T>(string originalString, ITypeConverter<string, T> converter);
@@ -271,21 +262,5 @@ namespace Cosmos.Guava
         List<T> SplitToList<T>(string originalString, IObjectSerializer serializer);
         List<T> SplitToList<T>(string originalString, ITypeConverter<string, T> converter);
         List<T> SplitToList<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper);
-
-    }
-
-    public interface IKeyValuePairSplitter
-    {
-        IKeyValuePairSplitter TrimResults();
-        IKeyValuePairSplitter TrimResults(Func<string, string> keyTrimFunc, Func<string, string> valueTrimFunc);
-        IKeyValuePairSplitter Limit(int limit);
-        IEnumerable<KeyValuePair<string, string>> Split(string originalString);
-        IEnumerable<KeyValuePair<string, T>> Split<T>(string originalString, IObjectSerializer serializer);
-        IEnumerable<KeyValuePair<string, T>> Split<T>(string originalString, ITypeConverter<string, T> converter);
-        IEnumerable<KeyValuePair<string, T>> Split<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper);
-        Dictionary<string, string> SplitToDictionary(string originalString);
-        Dictionary<string, T> SplitToDictionary<T>(string originalString, IObjectSerializer serializer);
-        Dictionary<string, T> SplitToDictionary<T>(string originalString, ITypeConverter<string, T> converter);
-        Dictionary<string, T> SplitToDictionary<TMiddle, T>(string originalString, IObjectSerializer serializer, IObjectMapper mapper);
     }
 }
