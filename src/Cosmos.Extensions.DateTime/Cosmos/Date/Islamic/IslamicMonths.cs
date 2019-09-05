b85afacbef9ff29b8eef8349d54df3e836ac9cac
alexinea <forerunner@outlook.com>
2019-09-05 10:22:29 +0800

    Add Islamic DateTime and DateInfo support.

---
+++ src/Cosmos.Extensions.DateTime/Cosmos/Date/Islamic/IslamicMonths.cs
@@ -0,0 +1,72 @@
+namespace Cosmos.Date.Islamic
+{
+    /// <summary>
+    /// Islamic / Hijri Months
+    /// </summary>
+    /// <remarks>
+    /// Reference to:
+    ///     https://en.wikipedia.org/wiki/Islamic_calendar
+    /// </remarks>
+    public enum IslamicMonths
+    {
+        /// <summary>
+        /// Muharram/Mohram
+        /// </summary>
+        Muharram = 1,
+
+        /// <summary>
+        /// Saphar/Safar
+        /// </summary>
+        Saphar = 2,
+
+        /// <summary>
+        /// Rabi al-Awwal
+        /// </summary>
+        RabiAlAwwal = 3,
+
+        /// <summary>
+        /// Rabi al-Thani (Rabī’ al-Ākhir)
+        /// </summary>
+        RabiAlThani = 4,
+
+        /// <summary>
+        /// Jomada Al Awwel/Jumada al-Ula
+        /// </summary>
+        JomadaAlAwwel = 5,
+
+        /// <summary>
+        /// Jomada Al Thani/Jumada al-Akhirah
+        /// </summary>
+        JomadaAlThani = 6,
+
+        /// <summary>
+        /// Rajab/Rojab
+        /// </summary>
+        Rajab = 7,
+
+        /// <summary>
+        /// Sha'ban/Shaban
+        /// </summary>
+        Shaban = 8,
+        
+        /// <summary>
+        /// Ramadan
+        /// </summary>
+        Ramadan = 9,
+        
+        /// <summary>
+        /// Shawwal
+        /// </summary>
+        Shawwal = 10,
+        
+        /// <summary>
+        /// Dul Qa'dah/Zulqiddah
+        /// </summary>
+        DulQaDah = 11,
+        
+        /// <summary>
+        /// Dul Hijjah/Zulhijjah
+        /// </summary>
+        DulHijjah = 12
+    }
+}
