8d16de56292ce671c721db2224875864797c5db4
alexinea <forerunner@outlook.com>
2019-12-16 13:57:14 +0800

    Update Tasks utility

--- src/Cosmos.Extensions.Asyncs/Cosmos/Asynchronous/Tasks.cs
+++ src/Cosmos.Extensions.Asyncs/Cosmos/Asynchronous/Tasks.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cosmos.Asynchronous {
@@ -5,7 +7,11 @@ namespace Cosmos.Asynchronous {
     /// Task utilities
     /// </summary>
     public static class Tasks {
+
+        #region CompletedTask
+
 #if NET451
+        // ReSharper disable once InconsistentNaming
         private static readonly Task _completedTask = Task.FromResult(true);
 #endif
         /// <summary>
@@ -19,5 +25,65 @@ namespace Cosmos.Asynchronous {
             return Task.CompletedTask;
 #endif
         }
+
+        #endregion
+
+        #region From Canceled
+
+        /// <summary>
+        /// From canceled
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        public static Task<TResult> FromCanceled<TResult>(CancellationToken cancellationToken) {
+#if NET451
+            var tcs = new TaskCompletionSource<TResult>();
+            tcs.SetCanceled();
+            return tcs.Task;
+#else
+            return Task.FromCanceled<TResult>(cancellationToken);
+#endif
+        }
+
+        /// <summary>
+        /// From canceled
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        public static Task<TResult> FromCanceled<TResult>(OperationCanceledException exception) {
+#if NET451
+            var tcs = new TaskCompletionSource<TResult>();
+            tcs.SetException(exception);
+            tcs.SetCanceled();
+            return tcs.Task;
+#else
+            return Task.FromCanceled<TResult>(exception.CancellationToken);
+#endif
+        }
+
+        #endregion
+
+        #region From Exception
+
+        /// <summary>
+        /// From exception
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        public static Task<TResult> FromException<TResult>(Exception exception) {
+#if NET451
+            var tcs = new TaskCompletionSource<TResult>();
+            tcs.TrySetException(exception);
+            return tcs.Task;
+#else
+            return Task.FromException<TResult>(exception);
+#endif
+        }
+
+        #endregion
+
     }
 }
