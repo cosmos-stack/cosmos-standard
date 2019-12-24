bb396da1375a5ee91232f3b303fc17d6f24e29c9
alexinea <forerunner@outlook.com>
2019-12-24 15:41:11 +0800

    Add Try extensions

--- src/Cosmos.Extensions/Cosmos/TryExtensions.cs
+++ src/Cosmos.Extensions/Cosmos/TryExtensions.cs
@@ -1,5 +1,76 @@
+using System;
+
 namespace Cosmos {
-    public class TryExtensions {
-        
+    /// <summary>
+    /// Extensions for Try
+    /// </summary>
+    public static class TryExtensions {
+        /// <summary>
+        /// Select
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        public static Try<TResult> Select<TSource, TResult>(this Try<TSource> source, Func<TSource, TResult> selector) {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (selector is null)
+                throw new ArgumentNullException(nameof(selector));
+            return source.Bind(val => Try.LiftValue(selector(val)));
+        }
+
+        /// <summary>
+        /// Select many
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        public static Try<TResult> SelectMany<TSource, TResult>(this Try<TSource> source, Func<TSource, Try<TResult>> selector) {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (selector is null)
+                throw new ArgumentNullException(nameof(selector));
+            return source.Bind(selector);
+        }
+
+        /// <summary>
+        /// Select many
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="convert"></param>
+        /// <param name="selector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TIntermediate"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        public static Try<TResult> SelectMany<TSource, TIntermediate, TResult>(this Try<TSource> source,
+            Func<TSource, Try<TIntermediate>> convert, Func<TSource, TIntermediate, TResult> selector) {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (convert is null)
+                throw new ArgumentNullException(nameof(convert));
+            if (selector is null)
+                throw new ArgumentNullException(nameof(selector));
+            return source.Bind(val => convert(val).Select(interVal => selector(val, interVal)));
+        }
+
+        /// <summary>
+        /// Where
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <returns></returns>
+        public static Try<TSource> Where<TSource>(this Try<TSource> source, Func<TSource, bool> predicate) {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (predicate is null)
+                throw new ArgumentNullException(nameof(predicate));
+            return source.Bind(val => predicate(val) ? source : Try.LiftException<TSource>(new InvalidOperationException("Predicate not satisfied")));
+        }
     }
 }
--- src/Cosmos.Extensions/Cosmos/Try`1.cs
+++ src/Cosmos.Extensions/Cosmos/Try`1.cs
@@ -1,5 +1,123 @@
+using System;
+
 namespace Cosmos {
-    public class Try_1 {
-        
+    public abstract class Try<T> {
+        /// <summary>
+        /// Is failure
+        /// </summary>
+        public abstract bool IsFailure { get; }
+
+        /// <summary>
+        /// Is success
+        /// </summary>
+        public abstract bool IsSuccess { get; }
+
+        /// <summary>
+        /// Exception
+        /// </summary>
+        public abstract Exception Exception { get; }
+
+        /// <summary>
+        /// Value
+        /// </summary>
+        public abstract T Value { get; }
+
+        /// <summary>
+        /// ==
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(Try<T> left, Try<T> right) {
+            if (left is null && right is null)
+                return true;
+            if (left is null || right is null)
+                return false;
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// !=
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(Try<T> left, Try<T> right) {
+            return !(left == right);
+        }
+
+        /// <inheritdoc/>
+        public abstract override string ToString();
+
+        /// <inheritdoc/>
+        public abstract override bool Equals(object obj);
+
+        /// <inheritdoc/>
+        public abstract override int GetHashCode();
+
+        /// <summary>
+        /// Deconstruct
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="exception"></param>
+        public abstract void Deconstruct(out T value, out Exception exception);
+
+        /// <summary>
+        /// Exception as
+        /// </summary>
+        /// <typeparam name="TException"></typeparam>
+        /// <returns></returns>
+        public TException ExceptionAs<TException>() where TException : Exception => Exception as TException;
+
+        /// <summary>
+        /// Recover
+        /// </summary>
+        /// <param name="recoverFunction"></param>
+        /// <returns></returns>
+        public abstract Try<T> Recover(Func<Exception, T> recoverFunction);
+
+        /// <summary>
+        /// Recover with
+        /// </summary>
+        /// <param name="recoverFunction"></param>
+        /// <returns></returns>
+        public abstract Try<T> RecoverWith(Func<Exception, Try<T>> recoverFunction);
+
+        /// <summary>
+        /// Match
+        /// </summary>
+        /// <param name="whenValue"></param>
+        /// <param name="whenException"></param>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        public abstract TResult Match<TResult>(Func<T, TResult> whenValue, Func<Exception, TResult> whenException);
+
+        /// <summary>
+        /// Map
+        /// </summary>
+        /// <param name="map"></param>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        public Try<TResult> Map<TResult>(Func<T, TResult> map) {
+            if (map is null)
+                throw new ArgumentNullException(nameof(map));
+            return Bind(value => Try.Create(() => map(value)));
+        }
+
+        /// <summary>
+        /// Tap
+        /// </summary>
+        /// <param name="successFunction"></param>
+        /// <param name="failureFunction"></param>
+        /// <returns></returns>
+        public abstract Try<T> Tap(Action<T> successFunction = null, Action<Exception> failureFunction = null);
+
+        /// <summary>
+        /// Bind
+        /// </summary>
+        /// <param name="bind"></param>
+        /// <typeparam name="TResult"></typeparam>
+        /// <returns></returns>
+        internal abstract Try<TResult> Bind<TResult>(Func<T, Try<TResult>> bind);
     }
 }
