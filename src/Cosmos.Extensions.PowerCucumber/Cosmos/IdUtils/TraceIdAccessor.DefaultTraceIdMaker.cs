e2605e96f663707096bea35d1bb8da425b7a87d5
alexinea <forerunner@outlook.com>
2020-05-21 22:33:31 +0800

    Incorporate "IdUtils" into the core library #49

---
+++ src/Cosmos.Extensions.PowerCucumber/Cosmos/IdUtils/TraceIdAccessor.DefaultTraceIdMaker.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Cosmos.IdUtils
+{
+    /// <summary>
+    /// Default TraceIdMaker
+    /// </summary>
+    public class DefaultTraceIdMaker : ITraceIdMaker
+    {
+        /// <summary>
+        /// Create Id
+        /// </summary>
+        /// <returns></returns>
+        public string CreateId()
+        {
+            var now = DateTime.Now;
+            return $"{now:yyyyMMddHHmmddffff}{RandomIdProvider.Create(7)}{now.Ticks}";
+        }
+    }
+}
