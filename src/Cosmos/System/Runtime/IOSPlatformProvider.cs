c73f8020e081604b00b09c6021c73d7d31e9755a
alexinea <forerunner@outlook.com>
2020-07-26 23:32:58 +0800

    Add Os Platform helpers

---
+++ src/Cosmos/System/Runtime/IOSPlatformProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace System.Runtime
+{
+    /// <summary>
+    /// Interface of Operation System Platform information provider<br />
+    /// 用于获取 <see cref="OSPlatform"/> 信息的提供者程序接口
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public interface IOSPlatformProvider
+    {
+        /// <summary>
+        /// Get current operation system platform information<br />
+        /// 获取当前操作系统平台信息（<see cref="OSPlatform"/>）
+        /// </summary>
+        /// <returns></returns>
+        // ReSharper disable once InconsistentNaming
+        OSPlatform GetCurrentOSPlatform();
+    }
+}
