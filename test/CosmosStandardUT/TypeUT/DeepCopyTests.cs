cee08b00a7152c4f8fcdc7d4dc06a3e2a2f77f43
alexinea <forerunner@outlook.com>
2020-12-06 22:56:55 +0800

    Refactor TypeVisit.DeepCopy and add UT

---
+++ test/CosmosStandardUT/TypeUT/DeepCopyTests.cs
@@ -0,0 +1,30 @@
+﻿using Cosmos.Reflection;
+using CosmosStandardUT.Models;
+using Xunit;
+
+namespace CosmosStandardUT.TypeUT
+{
+    [Trait("TypeUT", "TypeVisit.DeepCopy")]
+    public class DeepCopyTests
+    {
+        [Fact(DisplayName = "DeepCopier Test")]
+        public void DeepCopierTest()
+        {
+            var original = new NormalWithAttrClass {Good = "Nice", Index = 99, Nice = "Good"};
+            var target = TypeVisit.DeepCopy(original, DeepCopyOptions.DeepCopier);
+            Assert.Same(original, original);
+            Assert.Same(target, target);
+            Assert.NotSame(original, target);
+        }
+
+        [Fact(DisplayName = "ExpressionCopier Test")]
+        public void ExpressionCopierTest()
+        {
+            var original = new NormalWithAttrClass {Good = "Nice", Index = 99, Nice = "Good"};
+            var target = TypeVisit.DeepCopy(original, DeepCopyOptions.ExpressionCopier);
+            Assert.Same(original, original);
+            Assert.Same(target, target);
+            Assert.NotSame(original, target);
+        }
+    }
+}
