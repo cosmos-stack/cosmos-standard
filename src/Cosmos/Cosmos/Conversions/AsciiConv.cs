fe3049670d15f387c5723946f88cf4ab6d619110
alexinea <forerunner@outlook.com>
2020-12-12 22:19:04 +0800

    Refactor AsciiConv and add UT

---
+++ src/Cosmos/Cosmos/Conversions/AsciiConv.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace Cosmos.Conversions
+{
+    internal static class AsciiConvHelper
+    {
+        public static byte CharToAscii(char @char)
+        {
+            return (byte) @char;
+        }
+
+        public static byte[] StringToAscii(string @string)
+        {
+            return Encoding.ASCII.GetBytes(@string);
+        }
+
+        public static char AsciiToChar(byte @byte)
+        {
+            return (char) @byte;
+        }
+
+        public static string AsciiToString(byte[] bytes)
+        {
+            return Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+        }
+    }
+
+    public static class AsciiConv
+    {
+        /// <summary>
+        /// Convert from bytes to ASCII <see cref="string"/>.
+        /// </summary>
+        /// <example>in: new byte[] {65, 66, 67}; out: ABC</example>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static string BytesToAsciiString(byte[] bytes)
+        {
+            return AsciiConvHelper.AsciiToString(bytes);
+        }
+
+        /// <summary>
+        /// Convert from ASCII <see cref="string"/> to bytes.
+        /// </summary>
+        /// <example>in: ABC; out: new byte[] {65, 66, 67}</example>
+        /// <param name="asciiStr"></param>
+        /// <returns></returns>
+        public static byte[] AsciiStringToBytes(string asciiStr)
+        {
+            return AsciiConvHelper.StringToAscii(asciiStr);
+        }
+    }
+}
---
+++ test/CosmosStandardUT/ConvUT/AsciiConvTests.cs
@@ -0,0 +1,26 @@
+﻿using Cosmos.Conversions;
+using Shouldly;
+using Xunit;
+
+namespace CosmosStandardUT.ConvUT
+{
+    [Trait("ConvUT", "Ascii")]
+    public class AsciiConvTests
+    {
+        [Fact(DisplayName = "Bytes-2-AsciiString test")]
+        public void BytesAndAsciiStringTest()
+        {
+            var bytes = new byte[] {65, 66, 67};
+            var asciiString = AsciiConv.BytesToAsciiString(bytes);
+
+            asciiString.ShouldBe("ABC");
+
+            var byteArray = AsciiConv.AsciiStringToBytes(asciiString);
+
+            byteArray.Length.ShouldBe(3);
+            byteArray[0].ShouldBe((byte) 65);
+            byteArray[1].ShouldBe((byte) 66);
+            byteArray[2].ShouldBe((byte) 67);
+        }
+    }
+}
