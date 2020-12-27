a78aeab14c95250d6b1124a9dab33d89f5043ac7
alexinea <forerunner@outlook.com>
2020-12-27 15:39:51 +0800

    Refactor OptionalDictsExtensions and add UT.

--- src/Cosmos.Extensions.Collections/Cosmos/Optionals/OptionalDictsExtensions.cs
+++ src/Cosmos.Extensions.Collections/Cosmos/Optionals/OptionalDictsExtensions.cs
@@ -21,22 +21,6 @@ namespace Cosmos.Optionals
                 throw new ArgumentNullException(nameof(dictionary));
             return dictionary.TryGetValue(key, out var value) ? value : (TValue?) null;
         }
-
-        /// <summary>
-        /// Try get value
-        /// </summary>
-        /// <param name="dictionary"></param>
-        /// <param name="key"></param>
-        /// <typeparam name="TKey"></typeparam>
-        /// <typeparam name="TValue"></typeparam>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static TValue? GetOptionalValue<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key) where TValue : struct
-        {
-            if (dictionary is null)
-                throw new ArgumentNullException(nameof(dictionary));
-            return dictionary.TryGetValue(key, out var value) ? value : (TValue?) null;
-        }
         
         /// <summary>
         /// Try get value cascading
@@ -46,20 +30,7 @@ namespace Cosmos.Optionals
         /// <typeparam name="TKey"></typeparam>
         /// <typeparam name="TValue"></typeparam>
         /// <returns></returns>
-        public static TValue? GetOptionalValueCascading<TKey, TValue>(this IEnumerable<IDictionary<TKey, TValue>> dictionaryColl, TKey key) where TValue : struct
-        {
-            return dictionaryColl.TryGetValueCascading(key, out var value) ? value : (TValue?) null;
-        }
-        
-        /// <summary>
-        /// Try get value cascading
-        /// </summary>
-        /// <param name="dictionaryColl"></param>
-        /// <param name="key"></param>
-        /// <typeparam name="TKey"></typeparam>
-        /// <typeparam name="TValue"></typeparam>
-        /// <returns></returns>
-        public static TValue? GetOptionalValueCascading<TKey, TValue>(this IEnumerable<IReadOnlyDictionary<TKey, TValue>> dictionaryColl, TKey key) where TValue : struct
+        public static TValue? GetOptionalValue<TKey, TValue>(this IEnumerable<IDictionary<TKey, TValue>> dictionaryColl, TKey key) where TValue : struct
         {
             return dictionaryColl.TryGetValueCascading(key, out var value) ? value : (TValue?) null;
         }
---
+++ test/CosmosStandardUT/OptionalsUT/DictOptionalTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Cosmos.Optionals;
+using Shouldly;
+using Xunit;
+
+namespace CosmosStandardUT.OptionalsUT
+{
+    [Trait("CollUT", "DictUT.Optionals")]
+    public class DictOptionalTests
+    {
+        public DictOptionalTests()
+        {
+            Dictionary = new Dictionary<string, int> {{"A", 1}, {"B", 2}};
+
+            CascadingDictionary = new List<Dictionary<string, int>>
+            {
+                new() {{"A", 1}, {"B", 2}},
+                new() {{"C", 3}, {"D", 4}}
+            };
+        }
+
+        private Dictionary<string, int> Dictionary { get; }
+        private IEnumerable<Dictionary<string, int>> CascadingDictionary { get; }
+
+
+        [Fact(DisplayName = "extension method To getting optional value from dictionary test")]
+        public void GettingOptionalValueTest()
+        {
+            var val1 = Dictionary.GetOptionalValue("A");
+            var val2 = Dictionary.GetOptionalValue("B");
+            var val3 = Dictionary.GetOptionalValue("C");
+
+            val1.HasValue.ShouldBeTrue();
+            val2.HasValue.ShouldBeTrue();
+            val3.HasValue.ShouldBeFalse();
+
+            val1!.Value.ShouldBe(1);
+            val2!.Value.ShouldBe(2);
+        }
+
+        [Fact(DisplayName = "extension method To getting optional value from cascading dictionary test")]
+        public void GettingOptionalValueCascadingTest()
+        {
+            var val1 = CascadingDictionary.GetOptionalValue("A");
+            var val2 = CascadingDictionary.GetOptionalValue("B");
+            var val3 = CascadingDictionary.GetOptionalValue("C");
+            var val4 = CascadingDictionary.GetOptionalValue("D");
+            var val5 = CascadingDictionary.GetOptionalValue("E");
+
+            val1.HasValue.ShouldBeTrue();
+            val2.HasValue.ShouldBeTrue();
+            val3.HasValue.ShouldBeTrue();
+            val4.HasValue.ShouldBeTrue();
+            val5.HasValue.ShouldBeFalse();
+
+            val1!.Value.ShouldBe(1);
+            val2!.Value.ShouldBe(2);
+            val3!.Value.ShouldBe(3);
+            val4!.Value.ShouldBe(4);
+        }
+    }
+}
