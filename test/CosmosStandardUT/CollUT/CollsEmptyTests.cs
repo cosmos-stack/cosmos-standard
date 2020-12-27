60f5e7a93c06acfac9ec43fd6ea89686742830c0
alexinea <forerunner@outlook.com>
2020-12-27 21:52:05 +0800

    Add UT for Colls/Empty

---
+++ test/CosmosStandardUT/CollUT/CollsEmptyTests.cs
@@ -0,0 +1,19 @@
+﻿using Cosmos.Collections;
+using Shouldly;
+using Xunit;
+
+namespace CosmosStandardUT.CollUT
+{
+    [Trait("CollUT", "ListUT.Empty")]
+    public class CollsEmptyTests
+    {
+        [Fact(DisplayName = "Colls.Empty test")]
+        public void EmptyArrayTest()
+        {
+            var array = Colls.Empty<string>();
+
+            array.ShouldNotBeNull();
+            array.ShouldBeEmpty();
+        }
+    }
+}
