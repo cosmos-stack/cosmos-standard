4719b0fb9d574ad4ede116d3029dfdcc0501309a
alexinea <forerunner@outlook.com>
2020-12-27 14:58:38 +0800

    Refactor CollJudge/CollGuard utilities

--- src/Cosmos.Extensions.Validation.Basic/Cosmos/Collections/CollGuard.cs
+++ src/Cosmos.Extensions.Validation.Basic/Cosmos/Collections/CollGuard.cs
@@ -5,7 +5,7 @@ using Cosmos.Validation.Internals;
 
 namespace Cosmos.Collections
 {
-    public static class CollectionGuard
+    public static class CollGuard
     {
         /// <summary>
         /// Check if the collection is empty or null. <br />
@@ -16,7 +16,7 @@ namespace Cosmos.Collections
         /// <param name="argument"></param>
         /// <param name="argumentName"></param>
         /// <param name="message"></param>
-        public static void NotNull(IEnumerable argument, string argumentName, string message = null)
+        public static void ShouldBeNotNull(IEnumerable argument, string argumentName, string message = null)
         {
             ValidationExceptionHelper.WrapAndRaise<ArgumentNullException>(
                 argument != null,
@@ -32,10 +32,10 @@ namespace Cosmos.Collections
         /// <param name="argument"></param>
         /// <param name="argumentName"></param>
         /// <param name="message"></param>
-        public static void NotEmpty(IEnumerable argument, string argumentName, string message = null)
+        public static void ShouldBeNotEmpty(IEnumerable argument, string argumentName, string message = null)
         {
             ValidationExceptionHelper.WrapAndRaise<ArgumentNullException>(
-                !CollectionJudge.IsNullOrEmpty(argument),
+                !CollJudge.IsNullOrEmpty(argument),
                 argumentName, message ?? $"{nameof(argument)} can not be null or empty.");
         }
 
@@ -50,7 +50,7 @@ namespace Cosmos.Collections
         /// <param name="argumentName"></param>
         /// <param name="number"></param>
         /// <param name="message"></param>
-        public static void ContainsAtLeast<T>(ICollection<T> argument, int number, string argumentName, string message = null)
+        public static void ShouldContainAtLeast<T>(ICollection<T> argument, int number, string argumentName, string message = null)
         {
             ValidationExceptionHelper.WrapAndRaise<ArgumentOutOfRangeException>(
                 argument.Count >= number,
@@ -68,11 +68,61 @@ namespace Cosmos.Collections
         /// <param name="argument"></param>
         /// <param name="argumentName"></param>
         /// <param name="message"></param>
-        public static void NotNull<TKey, TValue>(KeyValuePair<TKey, TValue> argument, string argumentName, string message = null)
+        public static void ShouldBeNotNull<TKey, TValue>(KeyValuePair<TKey, TValue> argument, string argumentName, string message = null)
         {
             ValidationExceptionHelper.WrapAndRaise<ArgumentNullException>(
                 !string.IsNullOrWhiteSpace(argument.ToString()),
                 argumentName, message ?? $"{nameof(argument)} contains nothing.");
         }
+    }
+
+    public static class CollGuardExtensions
+    {
+        /// <summary>
+        /// Check if the collection is empty or null. <br />
+        /// If the collection is empty or null, an exception is thrown. <br />
+        /// 检查集合是否为空。 <br />
+        /// 如果集合为空，则抛出异常。
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <param name="argumentName"></param>
+        /// <param name="message"></param>
+        public static void Require(this IEnumerable argument, string argumentName, string message = null)
+        {
+            CollGuard.ShouldBeNotNull(argument, argumentName, message);
+        }
+        
+        /// <summary>
+        /// Check if the key/value pair is empty or null. <br />
+        /// If the key/value pair is empty or null, an exception is thrown. <br />
+        /// 检查键值对是否为空。 <br />
+        /// 如果键值对为空，则抛出异常。
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="argument"></param>
+        /// <param name="argumentName"></param>
+        /// <param name="message"></param>
+        public static void Require<TKey, TValue>(this KeyValuePair<TKey, TValue> argument, string argumentName, string message = null)
+        {
+            CollGuard.ShouldBeNotNull(argument, argumentName, message);
+        }
+        
+        /// <summary>
+        /// Check whether the set contains at least the specified number of elements. <br />
+        /// If the set is less than the specified number of elements, an exception is thrown. <br />
+        /// 检查集合是否包含至少指定个数的元素。 <br />
+        /// 如果集合少于指定数目的元素，则抛出异常。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="argument"></param>
+        /// <param name="argumentName"></param>
+        /// <param name="number"></param>
+        /// <param name="message"></param>
+        public static void RequireAtLeast<T>(this ICollection<T> argument, int number, string argumentName, string message = null)
+        {
+            CollGuard.ShouldContainAtLeast(argument, number, argumentName, message);
+        }
+
     }
 }
--- src/Cosmos.Extensions.Validation.Basic/Cosmos/Collections/CollJudge.cs
+++ src/Cosmos.Extensions.Validation.Basic/Cosmos/Collections/CollJudge.cs
@@ -1,10 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Cosmos.Collections
 {
     public static class CollJudge
     {
+        #region IsNull
+
+        /// <summary>
+        /// To judge whether the collection is null or not.<br />
+        /// 判断集合是否为空。
+        /// </summary>
+        /// <param name="coll"></param>
+        /// <returns></returns>
+        public static bool IsNull(IEnumerable coll)
+        {
+            return coll is null;
+        }
+
+        #endregion
+
+        #region IsNullOrEmpty
+
+        /// <summary>
+        /// Determine whether the collection is empty or empty.<br />
+        /// 判断集合是否为空。
+        /// </summary>
+        /// <param name="coll"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(IEnumerable coll)
+        {
+            return coll is null || !coll.Cast<object>().Any();
+        }
+
+        /// <summary>
+        /// Determine whether the collection is empty or empty.<br />
+        /// 判断集合是否为空。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="coll"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty<T>(IEnumerable<T> coll)
+        {
+            return coll is null || !coll.Any();
+        }
+
+        #endregion
+
+        #region IsSameCount
+        
         /// <summary>
         /// Check if the number of elements in the set is the same. <br />
         /// 检查两个集合是否拥有相等数量的成员
@@ -30,7 +75,7 @@ namespace Cosmos.Collections
         /// <param name="source"></param>
         /// <param name="that"></param>
         /// <returns></returns>
-        public static bool IsSameCount<T>(this IQueryable<T> source, IQueryable<T> that)
+        public static bool IsSameCount<T>(IQueryable<T> source, IQueryable<T> that)
         {
             if (source is null && that is null)
                 return true;
@@ -38,6 +83,8 @@ namespace Cosmos.Collections
                 return false;
             return source.Count().Equals(that.Count());
         }
+
+        #endregion
     }
 
     public static class CollJudgeExtensions
