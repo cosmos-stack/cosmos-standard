c5ef5af8a1e2f118dfc87703cae79e7df2804c8e
alexinea <forerunner@outlook.com>
2020-12-28 22:21:38 +0800

    Update Colls/CollConv.

--- src/Cosmos.Extensions.Collections/Cosmos/Collections/CollConv.cs
+++ src/Cosmos.Extensions.Collections/Cosmos/Collections/CollConv.cs
@@ -169,82 +169,6 @@ namespace Cosmos.Collections
         }
 
         #endregion
-
-        #region ToHashSet
-
-#if NETFRAMEWORK || NETSTANDARD2_0
-        /// <summary>
-        /// To hashset
-        /// </summary>
-        /// <param name="source"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source) where T : IComparable<T>
-        {
-            return source.ToHashSet(EqualityComparer<T>.Default);
-        }
-
-        /// <summary>
-        /// To hashset
-        /// </summary>
-        /// <param name="source"></param>
-        /// <param name="comparer"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer)
-            where T : IComparable<T>
-        {
-            if (source is null)
-                throw new ArgumentNullException(nameof(source));
-            if (comparer is null)
-                throw new ArgumentNullException(nameof(comparer));
-            return new HashSet<T>(source, comparer);
-        }
-
-#endif
-
-        /// <summary>
-        /// To HashSet
-        /// </summary>
-        /// <param name="source"></param>
-        /// <param name="ignoreDup"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, bool ignoreDup) where T : IComparable<T>
-        {
-            return ignoreDup
-                ? source.Distinct().ToHashSet(EqualityComparer<T>.Default)
-                : source.ToHashSet(EqualityComparer<T>.Default);
-        }
-
-        /// <summary>
-        /// To HashSet
-        /// </summary>
-        /// <param name="source"></param>
-        /// <param name="keyFunc"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="TKey"></typeparam>
-        /// <returns></returns>
-        public static HashSet<TKey> ToHashSet<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keyFunc)
-            where TKey : IComparable<TKey>
-        {
-            if (keyFunc is null) throw new ArgumentNullException(nameof(keyFunc));
-            return source.Select(keyFunc).ToHashSet(EqualityComparer<TKey>.Default);
-        }
-
-        /// <summary>
-        /// To HashSet ignore duplicates
-        /// </summary>
-        /// <param name="source"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static HashSet<T> ToHashSetIgnoringDuplicates<T>(this IEnumerable<T> source) where T : IComparable<T>
-        {
-            return source.ToHashSet(true);
-        }
-
-        #endregion
     }
 
     public static class CollConvExtensions
@@ -317,6 +241,86 @@ namespace Cosmos.Collections
             where TSource : IComparable<TSource>
         {
             return CollConv.ToSortedArray(source);
+        }
+
+        #endregion
+        
+        #region ToHashSet
+
+#if NETFRAMEWORK || NETSTANDARD2_0
+
+#if !NET472 && !NET48
+        
+        /// <summary>
+        /// To hashset
+        /// </summary>
+        /// <param name="source"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source) where T : IComparable<T>
+        {
+            return source.ToHashSet(EqualityComparer<T>.Default);
+        }
+#endif
+
+        /// <summary>
+        /// To hashset
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="comparer"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer)
+            where T : IComparable<T>
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (comparer is null)
+                throw new ArgumentNullException(nameof(comparer));
+            return new HashSet<T>(source, comparer);
+        }
+
+#endif
+
+        /// <summary>
+        /// To HashSet
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="ignoreDup"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, bool ignoreDup) where T : IComparable<T>
+        {
+            return ignoreDup
+                ? source.Distinct().ToHashSet(EqualityComparer<T>.Default)
+                : source.ToHashSet(EqualityComparer<T>.Default);
+        }
+
+        /// <summary>
+        /// To HashSet
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="keyFunc"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <returns></returns>
+        public static HashSet<TKey> ToHashSet<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keyFunc)
+            where TKey : IComparable<TKey>
+        {
+            if (keyFunc is null) throw new ArgumentNullException(nameof(keyFunc));
+            return source.Select(keyFunc).ToHashSet(EqualityComparer<TKey>.Default);
+        }
+
+        /// <summary>
+        /// To HashSet ignore duplicates
+        /// </summary>
+        /// <param name="source"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static HashSet<T> ToHashSetIgnoringDuplicates<T>(this IEnumerable<T> source) where T : IComparable<T>
+        {
+            return source.ToHashSet(true);
         }
 
         #endregion
--- src/Cosmos.Extensions.Collections/Cosmos/Collections/Colls.Ops.cs
+++ src/Cosmos.Extensions.Collections/Cosmos/Collections/Colls.Ops.cs
@@ -441,33 +441,4 @@ namespace Cosmos.Collections
 
         #endregion
     }
-
-    /// <summary>
-    /// Set and range result
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class SetAddRangeResult<T>
-    {
-        /// <summary>
-        /// Create a new instance of <see cref="SetAddRangeResult{T}"/>
-        /// </summary>
-        /// <param name="item"></param>
-        /// <param name="added"></param>
-        public SetAddRangeResult(T item, bool added)
-        {
-            // Set values.
-            Item = item;
-            Added = added;
-        }
-
-        /// <summary>
-        /// Gets item
-        /// </summary>
-        public T Item { get; }
-
-        /// <summary>
-        /// Addod or not...
-        /// </summary>
-        public bool Added { get; }
-    }
 }
