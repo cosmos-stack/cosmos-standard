95fc2726ac9ee419da86f43b9906d0fd34b047eb
alexinea <forerunner@outlook.com>
2020-12-29 16:48:38 +0800

    Refactor Base32/64, add BaseConv and UT.

---
+++ src/Cosmos/Cosmos/Conversions/BaseConv.cs
@@ -0,0 +1,128 @@
+﻿using System.Text;
+using Cosmos.Text.Internal;
+
+namespace Cosmos.Conversions
+{
+    public static class BaseConv
+    {
+        public static string ToBase32( byte[] data)
+        {
+            var base32 = new Base32();
+            return base32.Encode(data);
+        }
+        
+        public static  byte[] FromBase32(string data)
+        {
+            var base32 = new Base32();
+            return base32.Decode(data);
+        }
+        
+        public static string ToBase32String(string data, Encoding encoding = null)
+        {
+            var base32 = new Base32(textEncoding: encoding);
+            return base32.EncodeString(data);
+        }
+        
+        public static string FromBase32String(string data, Encoding encoding = null)
+        {
+            var base32 = new Base32(textEncoding: encoding);
+            return base32.DecodeToString(data);
+        }
+        
+        public static string ToZBase32( byte[] data)
+        {
+            var base32 = new ZBase32();
+            return base32.Encode(data);
+        }
+        
+        public static  byte[] FromZBase32(string data)
+        {
+            var base32 = new ZBase32();
+            return base32.Decode(data);
+        }
+        
+        public static string ToZBase32String(string data, Encoding encoding = null)
+        {
+            var base32 = new ZBase32(textEncoding: encoding);
+            return base32.EncodeString(data);
+        }
+        
+        public static string FromZBase32String(string data, Encoding encoding = null)
+        {
+            var base32 = new ZBase32(textEncoding: encoding);
+            return base32.DecodeToString(data);
+        }
+        
+        public static string ToBase64( byte[] data)
+        {
+            var base64 = new Base64();
+            return base64.Encode(data);
+        }
+        
+        public static  byte[] FromBase64(string data)
+        {
+            var base64 = new Base64();
+            return base64.Decode(data);
+        }
+        
+        public static string ToBase64String(string data, Encoding encoding = null)
+        {
+            var base64 = new Base64(textEncoding: encoding);
+            return base64.EncodeString(data);
+        }
+
+        public static string FromBase64String(string data, Encoding encoding = null)
+        {
+            var base64 = new Base64(textEncoding: encoding);
+            return base64.DecodeToString(data);
+        }
+        
+        public static string ToBase91( byte[] data)
+        {
+            var base91 = new Base91();
+            return base91.Encode(data);
+        }
+        
+        public static  byte[] FromBase91(string data)
+        {
+            var base91 = new Base91();
+            return base91.Decode(data);
+        }
+        
+        public static string ToBase91String(string data, Encoding encoding = null)
+        {
+            var base91 = new Base91(textEncoding: encoding);
+            return base91.EncodeString(data);
+        }
+
+        public static string FromBase91String(string data, Encoding encoding = null)
+        {
+            var base91 = new Base91(textEncoding: encoding);
+            return base91.DecodeToString(data);
+        }
+        
+        public static string ToBase256( byte[] data)
+        {
+            var base256 = new Base256();
+            return base256.Encode(data);
+        }
+        
+        public static  byte[] FromBase256(string data)
+        {
+            var base256 = new Base256();
+            return base256.Decode(data);
+        }
+        
+        public static string ToBase256String(string data, Encoding encoding = null)
+        {
+            var base256 = new Base256(textEncoding: encoding);
+            return base256.EncodeString(data);
+        }
+
+        public static string FromBase256String(string data, Encoding encoding = null)
+        {
+            var base256 = new Base256(textEncoding: encoding);
+            return base256.DecodeToString(data);
+        }
+    }
+}
---
+++ test/CosmosStandardUT/StringUT/BaseXTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+using Cosmos.Conversions;
+using Xunit;
+
+namespace CosmosStandardUT.StringUT
+{
+    [Trait("ConvUT", "BaseX")]
+    public class BaseXTests
+    {
+        [Fact(DisplayName = "Base32 string test")]
+        public void Base32StringTest()
+        {
+            var baseVal = BaseConv.ToBase32String("Alex LEWIS");
+            var originalVal = BaseConv.FromBase32String(baseVal);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+
+        [Fact(DisplayName = "Base32 byte[] test")]
+        public void Base32BytesTest()
+        {
+            var byteArray = Encoding.UTF8.GetBytes("Alex LEWIS");
+            var baseVal = BaseConv.ToBase32(byteArray);
+            var originalByteArray = BaseConv.FromBase32(baseVal);
+            var originalVal = Encoding.UTF8.GetString(originalByteArray);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+
+        [Fact(DisplayName = "Base64 string test")]
+        public void Base64StringTest()
+        {
+            var baseVal = BaseConv.ToBase64String("Alex LEWIS");
+            var originalVal = BaseConv.FromBase64String(baseVal);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+
+        [Fact(DisplayName = "Base64 byte[] test")]
+        public void Base64BytesTest()
+        {
+            var byteArray = Encoding.UTF8.GetBytes("Alex LEWIS");
+            var baseVal = BaseConv.ToBase64(byteArray);
+            var originalByteArray = BaseConv.FromBase64(baseVal);
+            var originalVal = Encoding.UTF8.GetString(originalByteArray);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+
+        [Fact(DisplayName = "Base91 string test")]
+        public void Base91StringTest()
+        {
+            var baseVal = BaseConv.ToBase91String("Alex LEWIS");
+            var originalVal = BaseConv.FromBase91String(baseVal);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+
+        [Fact(DisplayName = "Base91 byte[] test")]
+        public void Base91BytesTest()
+        {
+            var byteArray = Encoding.UTF8.GetBytes("Alex LEWIS");
+            var baseVal = BaseConv.ToBase91(byteArray);
+            var originalByteArray = BaseConv.FromBase91(baseVal);
+            var originalVal = Encoding.UTF8.GetString(originalByteArray);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+
+        [Fact(DisplayName = "Base256 string test")]
+        public void Base256StringTest()
+        {
+            var baseVal = BaseConv.ToBase256String("Alex LEWIS");
+            var originalVal = BaseConv.FromBase256String(baseVal);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+
+        [Fact(DisplayName = "Base256 byte[] test")]
+        public void Base256BytesTest()
+        {
+            var byteArray = Encoding.UTF8.GetBytes("Alex LEWIS");
+            var baseVal = BaseConv.ToBase256(byteArray);
+            var originalByteArray = BaseConv.FromBase256(baseVal);
+            var originalVal = Encoding.UTF8.GetString(originalByteArray);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+        
+        [Fact(DisplayName = "ZBase32 string test")]
+        public void ZBase32StringTest()
+        {
+            var baseVal = BaseConv.ToZBase32String("Alex LEWIS");
+            var originalVal = BaseConv.FromZBase32String(baseVal);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+
+        [Fact(DisplayName = "ZBase32 byte[] test")]
+        public void ZBase32BytesTest()
+        {
+            var byteArray = Encoding.UTF8.GetBytes("Alex LEWIS");
+            var baseVal = BaseConv.ToZBase32(byteArray);
+            var originalByteArray = BaseConv.FromZBase32(baseVal);
+            var originalVal = Encoding.UTF8.GetString(originalByteArray);
+
+            Assert.Equal("Alex LEWIS", originalVal);
+        }
+    }
+}
