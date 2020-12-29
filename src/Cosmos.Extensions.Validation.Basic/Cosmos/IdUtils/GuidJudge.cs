457302420d7ff87667ed102f9c5abc638ab9feee
alexinea <forerunner@outlook.com>
2020-12-29 14:57:40 +0800

    Refactor GuidJudge and add UT.

---
+++ src/Cosmos.Extensions.Validation.Basic/Cosmos/IdUtils/GuidJudge.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Cosmos.IdUtils
+{
+    public static class GuidJudge
+    {
+        /// <summary>
+        /// Determine whether the given nullable Guid is Null or Empty. <br />
+        /// 判断给定的可空 Guid 是否为 Null 或 Empty。
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(Guid? guid)
+        {
+            return guid is null || guid == Guid.Empty;
+        }
+
+        /// <summary>
+        /// Determine whether the given Guid is Null or Empty. <br />
+        /// 判断给定的 Guid 是否为 Null 或 Empty。
+        /// </summary>
+        /// <param name="guid"> 值 </param>
+        public static bool IsNullOrEmpty(Guid guid)
+        {
+            return guid == Guid.Empty;
+        }
+    }
+
+    public static class GuidJudgeExtensions
+    {
+        /// <summary>
+        /// Determine whether the given nullable Guid is Null or Empty. <br />
+        /// 判断给定的可空 Guid 是否为 Null 或 Empty。
+        /// </summary>
+        /// <param name="guid"> 值 </param>
+        public static bool IsNullOrEmpty(this Guid? guid)
+        {
+            return GuidJudge.IsNullOrEmpty(guid);
+        }
+
+        /// <summary>
+        /// Determine whether the given Guid is Null or Empty. <br />
+        /// 判断给定的 Guid 是否为 Null 或 Empty。
+        /// </summary>
+        /// <param name="guid"> 值 </param>
+        public static bool IsNullOrEmpty(this Guid guid)
+        {
+            return GuidJudge.IsNullOrEmpty(guid);
+        }
+    }
+}
---
+++ test/CosmosStandardUT/IdUtilsUT/GuidJudgeTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Cosmos.IdUtils;
+using Shouldly;
+using Xunit;
+
+namespace CosmosStandardUT.IdUtilsUT
+{
+    [Trait("IdUtilUT","Guid.Judge")]
+    public class GuidJudgeTests
+    {
+        [Fact(DisplayName = "Nullable Guid is empty or null test")]
+        public void NullableGuidEmptyOrNullTest()
+        {
+            Guid? guid1 = Guid.Empty;
+            Guid? guid2 = null;
+            Guid? guid3 = default;
+            
+            GuidJudge.IsNullOrEmpty(guid1).ShouldBeTrue();
+            GuidJudge.IsNullOrEmpty(guid2).ShouldBeTrue();
+            GuidJudge.IsNullOrEmpty(guid3).ShouldBeTrue();
+        }
+
+        [Fact(DisplayName = "Normal Guid is empty or null test")]
+        public void NormalGuidEmptyOrNullTest()
+        {
+            Guid guid1 = Guid.Empty;
+            Guid guid2 = default;
+            
+            GuidJudge.IsNullOrEmpty(guid1).ShouldBeTrue();
+            GuidJudge.IsNullOrEmpty(guid2).ShouldBeTrue();
+        }
+
+        [Fact(DisplayName = "Nullable Guid with value test")]
+        public void NullableGuidWithValueTest()
+        {
+            Guid? guid = Guid.NewGuid();
+            
+            GuidJudge.IsNullOrEmpty(guid).ShouldBeFalse();
+        }
+
+        [Fact(DisplayName = "Normal Guid with value test")]
+        public void NormalGuidWithValueTest()
+        {
+            Guid guid = Guid.NewGuid();
+            
+            GuidJudge.IsNullOrEmpty(guid).ShouldBeFalse();
+        }
+        
+        [Fact(DisplayName = "Extension method for Nullable Guid is empty or null test")]
+        public void ExtensionMethodForNullableGuidEmptyOrNullTest()
+        {
+            Guid? guid1 = Guid.Empty;
+            Guid? guid2 = null;
+            Guid? guid3 = default;
+            
+            guid1.IsNullOrEmpty().ShouldBeTrue();
+            guid2.IsNullOrEmpty().ShouldBeTrue();
+            guid3.IsNullOrEmpty().ShouldBeTrue();
+        }
+
+        [Fact(DisplayName = "Extension method for Normal Guid is empty or null test")]
+        public void ExtensionMethodForNormalGuidEmptyOrNullTest()
+        {
+            Guid guid1 = Guid.Empty;
+            Guid guid2 = default;
+            
+            guid1.IsNullOrEmpty().ShouldBeTrue();
+            guid2.IsNullOrEmpty().ShouldBeTrue();
+        }
+
+        [Fact(DisplayName = "Extension method for Nullable Guid with value test")]
+        public void ExtensionMethodForNullableGuidWithValueTest()
+        {
+            Guid? guid = Guid.NewGuid();
+            
+            guid.IsNullOrEmpty().ShouldBeFalse();
+        }
+
+        [Fact(DisplayName = "Extension method for Normal Guid with value test")]
+        public void ExtensionMethodForNormalGuidWithValueTest()
+        {
+            Guid guid = Guid.NewGuid();
+            
+            guid.IsNullOrEmpty().ShouldBeFalse();
+        }
+    }
+}
