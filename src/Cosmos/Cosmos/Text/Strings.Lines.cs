c4168cea33ccaf23d0c3fc4f99da922043d742ab
alexinea <forerunner@outlook.com>
2020-12-31 00:01:01 +0800

    Refactor Strings/Line and add UT.

---
+++ src/Cosmos/Cosmos/Text/Strings.Lines.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cosmos.Text
+{
+    /// <summary>
+    /// String Utils<br />
+    /// 字符串工具
+    /// </summary>
+    public static partial class Strings
+    {
+        /// <summary>
+        /// New line
+        /// </summary>
+        public const string NEWLINE = "\r\n";
+        
+        /// <summary>
+        /// Count by lines
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static int CountByLines(string text)
+        {
+            int index = 0, lines = 0;
+
+            while (true)
+            {
+                var newIndex = text.IndexOf(Environment.NewLine, index, StringComparison.Ordinal);
+                
+                if (newIndex < 0)
+                {
+                    if (text.Length > index)
+                        lines++;
+
+                    return lines;
+                }
+
+                index = newIndex + 2;
+                lines++;
+            }
+        }
+
+        /// <summary>
+        /// Split by lines
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> SplitByLines(string text)
+        {
+            var index = 0;
+
+            while (true)
+            {
+                var newIndex = text.IndexOf(Environment.NewLine, index, StringComparison.Ordinal);
+                if (newIndex < 0)
+                {
+                    if (text.Length > index)
+                        yield return text.Substring(index);
+
+                    yield break;
+                }
+
+                var currentString = text.Substring(index, newIndex - index);
+                index = newIndex + 2;
+
+                yield return currentString;
+            }
+        }
+    }
+
+    public static partial class StringsExtensions
+    {
+        /// <summary>
+        /// Line count
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static int CountByLines(this string text)
+        {
+            return Strings.CountByLines(text);
+        }
+
+        /// <summary>
+        /// To lines
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> SplitByLines(this string text)
+        {
+            return Strings.SplitByLines(text);
+        }
+    }
+}
---
+++ test/CosmosStandardUT/StringUT/StringLineTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Cosmos.Text;
+using Shouldly;
+using Xunit;
+
+namespace CosmosStandardUT.StringUT
+{
+    [Trait("StringUT","Strings.Lines")]
+    public class StringLineTests
+    {
+        private string TestSampleOne =@"The Apache License 2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an ""AS IS"" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"; // 6 lines
+
+        [Fact(DisplayName = "Count a text and returns the number of line test")]
+        public void LineCountTest()
+        {
+            Strings.CountByLines(TestSampleOne).ShouldBe(6);
+        }
+
+        [Fact(DisplayName = "Split a text and returns the all lines test")]
+        public void LineItemsTest()
+        {
+            var texts = Strings.SplitByLines(TestSampleOne).ToList();
+            
+            texts[0].ShouldBe("The Apache License 2.0");
+            texts[1].Trim().ShouldBe("Unless required by applicable law or agreed to in writing, software");
+        }
+        
+        [Fact(DisplayName = "Extensions method for Count a text and returns the number of line test")]
+        public void ExtensionMethodForLineCountTest()
+        {
+            TestSampleOne.CountByLines().ShouldBe(6);
+        }
+
+        [Fact(DisplayName = "Extensions method for Split a text and returns the all lines test")]
+        public void ExtensionMethodForLineItemsTest()
+        {
+            var texts = TestSampleOne.SplitByLines().ToList();
+            
+            texts[0].ShouldBe("The Apache License 2.0");
+            texts[1].Trim().ShouldBe("Unless required by applicable law or agreed to in writing, software");
+        }
+    }
+}
