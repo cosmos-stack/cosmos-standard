cc59b25a97dd48a7732f7035e79baefeb1fc06a7
alexinea <forerunner@outlook.com>
2021-01-06 16:20:36 +0800

    Refactor Strings/Is, StringJudge and StringGuard.

--- src/Cosmos.Extensions.Validation.Basic/Cosmos/Text/StringGuard.cs
+++ src/Cosmos.Extensions.Validation.Basic/Cosmos/Text/StringGuard.cs
@@ -20,7 +20,7 @@ namespace Cosmos.Text
                 !string.IsNullOrWhiteSpace(argument),
                 argumentName, message ?? $"{nameof(argument)} can not be null or white space.");
         }
-        
+
         /// <summary>
         /// Check if the string is empty or blank. <br />
         /// If the string is empty or blank, an exception is thrown. <br />
@@ -66,6 +66,43 @@ namespace Cosmos.Text
             ValidationExceptionHelper.WrapAndRaise<ArgumentOutOfRangeException>(
                 argument.Trim().Length <= length,
                 argumentName, argument.Trim().Length, message ?? $"{nameof(argument)}'s length can not be greater than {length}.");
+        }
+    }
+
+    public static class StringGuardExtensions
+    {
+        /// <summary>
+        /// 检查字符串是否为 null、String.Empty 或 Blank
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <param name="argumentName"></param>
+        /// <param name="message"></param>
+        public static void CheckNull(this string argument, string argumentName, string message = null)
+        {
+            StringGuard.NotNull(argument, argumentName, message);
+        }
+
+        /// <summary>
+        /// 检查字符串是否为 String.Empty 或 Blank
+        /// </summary>
+        /// <param name="argument"></param>
+        /// <param name="argumentName"></param>
+        /// <param name="message"></param>
+        public static void CheckBlank(this string argument, string argumentName, string message = null)
+        {
+            StringGuard.NotEmpty(argument, argumentName, message);
+        }
+
+        ///  <summary>
+        /// 检查字符串长度是否超界
+        ///  </summary>
+        ///  <param name="argument"></param>
+        ///  <param name="length"></param>
+        ///  <param name="argumentName"></param>
+        ///  <param name="message"></param>
+        public static void RequireMaxLength(this string argument, int length, string argumentName, string message = null)
+        {
+            StringGuard.NotOutOfLength(argument, length, argumentName, message);
         }
     }
 }
---
+++ src/Cosmos/Cosmos/Text/Strings.Is.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+
+namespace Cosmos.Text
+{
+    /// <summary>
+    /// String Utils<br />
+    /// 字符串工具
+    /// </summary>
+    public static partial class Strings
+    {
+        /// <summary>
+        /// Is upper
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsUpper( string text)
+        {
+            return FilterForLetters(text).All(char.IsUpper);
+        }
+
+        /// <summary>
+        /// Is lower
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsLower( string text)
+        {
+            return FilterForLetters(text).All(char.IsLower);
+        }
+    }
+
+    public static partial class StringsExtensions
+    {
+        /// <summary>
+        /// Is upper
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsUpper(this string text)
+        {
+            return Strings.IsUpper(text);
+        }
+
+        /// <summary>
+        /// Is lower
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsLower(this string text)
+        {
+            return Strings.IsLower(text);
+        }
+    }
+
+    public static partial class StringsShortcutExtensions
+    {
+        #region Is
+
+        /// <summary>
+        /// 检查字符串是 null 还是 System.String.Empty 字符串
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string text)
+        {
+            return string.IsNullOrEmpty(text);
+        }
+
+        /// <summary>
+        /// 检查字符串不是 null 或 System.String.Empty 字符串
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsNotNullNorEmpty(this string text)
+        {
+            return !text.IsNullOrEmpty();
+        }
+
+        /// <summary>
+        /// 检查字符串是 null、空还是仅由空白字符组成
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsNullOrWhiteSpace(this string text)
+        {
+            return string.IsNullOrWhiteSpace(text);
+        }
+
+        /// <summary>
+        /// 检查字符串不是 null、空或由空白字符串组成
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsNotNullNorWhiteSpace(this string text)
+        {
+            return !text.IsNullOrWhiteSpace();
+        }
+
+        #endregion
+    }
+}
