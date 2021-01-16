c929ca57d6ae50625e07e01ae432244bf4b143ea
alexinea <forerunner@outlook.com>
2021-01-17 01:29:13 +0800

    Add BooleanVal

--- src/Cosmos/Cosmos/BooleanUtilities.cs
+++ src/Cosmos/Cosmos/BooleanUtilities.cs
@@ -39,6 +39,19 @@ namespace Cosmos
         }
 
         /// <summary>
+        /// If true...
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="action"></param>
+        public static void IfTrue<T>(this BooleanVal<T> @this, Action<T> action)
+        {
+            if (@this.Value)
+            {
+                action?.Invoke(@this.Object);
+            }
+        }
+
+        /// <summary>
         /// If false...
         /// </summary>
         /// <param name="this"></param>
@@ -62,6 +75,19 @@ namespace Cosmos
             if (!@this)
             {
                 action?.Invoke(context);
+            }
+        }
+
+        /// <summary>
+        /// If true...
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="action"></param>
+        public static void IfFalse<T>(this BooleanVal<T> @this, Action<T> action)
+        {
+            if (!@this.Value)
+            {
+                action?.Invoke(@this.Object);
             }
         }
 
@@ -107,6 +133,45 @@ namespace Cosmos
             }
         }
 
+        /// <summary>
+        /// If this then that...
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <param name="this"></param>
+        /// <param name="that"></param>
+        public static void Ifttt<T>(this BooleanVal<T> condition, Action @this, Action that)
+        {
+            if (condition.Value)
+            {
+                @this?.Invoke();
+            }
+            else
+            {
+                that?.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// If this then that...
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="condition"></param>
+        /// <param name="this"></param>
+        /// <param name="that"></param>
+        /// <returns></returns>
+        public static TValue Ifttt<T, TValue>(this BooleanVal<T> condition, Func<T, TValue> @this, Func<T, TValue> @that)
+        {
+            if (condition.Value)
+            {
+                return @this is null ? default : @this.Invoke(condition.Object);
+            }
+            else
+            {
+                return that is null ? default : that.Invoke(condition.Object);
+            }
+        }
+
         #endregion
 
         #region If this then throw
@@ -122,6 +187,26 @@ namespace Cosmos
         }
 
         /// <summary>
+        /// If true then throw exception
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="exception"></param>
+        public static void IfTrueThenThrow<T>(this BooleanVal<T> @this, Exception exception)
+        {
+            @this.IfTrue(_ => ExceptionHelper.PrepareForRethrow(exception));
+        }
+
+        /// <summary>
+        /// If true then throw exception
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="exceptionFunc"></param>
+        public static void IfTrueThenThrow<T>(this BooleanVal<T> @this, Func<T, Exception> exceptionFunc)
+        {
+            @this.IfTrue(val => ExceptionHelper.PrepareForRethrow(exceptionFunc.Invoke(val)));
+        }
+
+        /// <summary>
         /// If false then throw exception
         /// </summary>
         /// <param name="this"></param>
@@ -129,6 +214,26 @@ namespace Cosmos
         public static void IfFalseThenThrow(this bool @this, Exception exception)
         {
             @this.IfFalse(() => ExceptionHelper.PrepareForRethrow(exception));
+        }
+
+        /// <summary>
+        /// If false then throw exception
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="exception"></param>
+        public static void IfFalseThenThrow<T>(this BooleanVal<T> @this, Exception exception)
+        {
+            @this.IfFalse(_ => ExceptionHelper.PrepareForRethrow(exception));
+        }
+
+        /// <summary>
+        /// If false then throw exception
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="exceptionFunc"></param>
+        public static void IfFalseThenThrow<T>(this BooleanVal<T> @this, Func<T, Exception> exceptionFunc)
+        {
+            @this.IfFalse(val => ExceptionHelper.PrepareForRethrow(exceptionFunc.Invoke(val)));
         }
 
         #endregion
@@ -197,6 +302,32 @@ namespace Cosmos
             return @this;
         }
 
+        public static BooleanVal<T> IfTrueThenInvoke<T>(this BooleanVal<T> @this, Func<bool> func)
+        {
+            if (@this.Value)
+            {
+                @this = func?.Invoke() ?? false;
+            }
+
+            return @this;
+        }
+
+        public static BooleanVal<T> IfTrueThenInvoke<T>(this BooleanVal<T> @this, Func<T, bool> func)
+        {
+            if (@this.Value)
+            {
+                @this = func?.Invoke(@this.Object) ?? false;
+            }
+
+            return @this;
+        }
+
+        public static BooleanVal<T> IfTrueThenInvoke<T>(this BooleanVal<T> @this, Action<T> action, T context)
+        {
+            @this.IfTrue(action);
+            return @this;
+        }
+
         public static bool IfFalseThenInvoke(this bool @this, Func<bool> func)
         {
             if (!@this)
@@ -226,6 +357,32 @@ namespace Cosmos
         public static bool IfFalseThenInvoke<T>(this bool @this, Action<T> action, T context)
         {
             @this.IfFalse(action, context);
+            return @this;
+        }
+
+        public static BooleanVal<T> IfFalseThenInvoke<T>(this BooleanVal<T> @this, Func<bool> func)
+        {
+            if (!@this.Value)
+            {
+                @this = func?.Invoke() ?? false;
+            }
+
+            return @this;
+        }
+
+        public static BooleanVal<T> IfFalseThenInvoke<T>(this BooleanVal<T> @this, Func<T, bool> func)
+        {
+            if (!@this.Value)
+            {
+                @this = func?.Invoke(@this.Object) ?? false;
+            }
+
+            return @this;
+        }
+
+        public static BooleanVal<T> IfFalseThenInvoke<T>(this BooleanVal<T> @this, Action<T> action, T context)
+        {
+            @this.IfFalse(action);
             return @this;
         }
 
@@ -280,6 +437,48 @@ namespace Cosmos
             if (!current) return false;
             return next?.Invoke() ?? false;
         }
+        
+        public static BooleanVal<T> And<T>(this bool current, Func<bool> next, T context)
+        {
+            if (!current) return false.ToBooleanVal(context);
+            return (next?.Invoke() ?? false).ToBooleanVal(context);;
+        }
+
+        public static BooleanVal<T> And<T>(this bool current, Func<(bool, T)> next)
+        {
+            if (!current) return false;
+            return next?.Invoke() ?? false.ToBooleanVal<T>();
+        }
+
+        public static BooleanVal<T> And<T>(this bool current, Func<BooleanVal<T>> next)
+        {
+            if (!current) return false;
+            return next?.Invoke() ?? false.ToBooleanVal<T>();
+        }
+
+        public static BooleanVal<T> And<T>(this bool current, Func<T, BooleanVal<T>> next, T context)
+        {
+            if (!current) return false.ToBooleanVal(context);
+            return next?.Invoke(context) ?? false.ToBooleanVal(context);
+        }
+
+        public static BooleanVal<T> And<T>(this BooleanVal<T> current, Func<(bool, T)> next)
+        {
+            if (!current.Value) return current;// means false
+            return next?.Invoke() ?? current;
+        }
+
+        public static BooleanVal<T> And<T>(this BooleanVal<T> current, Func<BooleanVal<T>> next)
+        {
+            if (!current.Value) return current;// means false
+            return next?.Invoke() ?? current;
+        }
+
+        public static BooleanVal<T> And<T>(this BooleanVal<T> current, Func<T, BooleanVal<T>> next)
+        {
+            if (!current.Value) return current;// means false
+            return next?.Invoke(current.Object) ?? current;
+        }
 
         public static bool And(this bool current, IEnumerable<Func<bool>> nextSet)
         {
@@ -296,10 +495,58 @@ namespace Cosmos
             return next;
         }
 
+        public static BooleanVal<T> And<T>(this bool current, bool next, T context)
+        {
+            if (!current) return false.ToBooleanVal(context);
+            return next.ToBooleanVal(context);
+        }
+
         public static bool Or(this bool current, Func<bool> next)
         {
             if (current) return true;
             return next?.Invoke() ?? false;
+        }
+
+        public static BooleanVal<T> Or<T>(this bool current, Func<bool> next, T context)
+        {
+            if (current) return true.ToBooleanVal(context);
+            return (next?.Invoke() ?? false).ToBooleanVal(context);
+        }
+
+        public static BooleanVal<T> Or<T>(this bool current, Func<(bool, T)> next)
+        {
+            if (current) return true;
+            return next?.Invoke() ?? false.ToBooleanVal<T>();
+        }
+
+        public static BooleanVal<T> Or<T>(this bool current, Func<BooleanVal<T>> next)
+        {
+            if (current) return true;
+            return next?.Invoke() ?? false.ToBooleanVal<T>();
+        }
+
+        public static BooleanVal<T> Or<T>(this bool current, Func<T, BooleanVal<T>> next, T context)
+        {
+            if (current) return true.ToBooleanVal(context);
+            return next?.Invoke(context) ?? false.ToBooleanVal(context);
+        }
+
+        public static BooleanVal<T> Or<T>(this BooleanVal<T> current, Func<(bool, T)> next)
+        {
+            if (current.Value) return current;// means true
+            return next?.Invoke() ?? false.ToBooleanVal(current.Object);
+        }
+
+        public static BooleanVal<T> Or<T>(this BooleanVal<T> current, Func<BooleanVal<T>> next)
+        {
+            if (current.Value) return current;// means true
+            return next?.Invoke() ?? false.ToBooleanVal(current.Object);
+        }
+
+        public static BooleanVal<T> Or<T>(this BooleanVal<T> current, Func<T, BooleanVal<T>> next)
+        {
+            if (current.Value) return current;// means true
+            return next?.Invoke(current.Object) ?? false.ToBooleanVal(current.Object);
         }
 
         public static bool Or(this bool current, IEnumerable<Func<bool>> nextSet)
@@ -315,6 +562,12 @@ namespace Cosmos
         {
             if (current) return true;
             return next;
+        }
+
+        public static BooleanVal<T> Or<T>(this bool current, bool next, T context)
+        {
+            if (current) return true.ToBooleanVal(context);
+            return next.ToBooleanVal(context);
         }
     }
 }
---
+++ src/Cosmos/Cosmos/BooleanVal.cs
@@ -0,0 +1,48 @@
+﻿namespace Cosmos
+{
+    /// <summary>
+    /// Boolean value wrapper
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public struct BooleanVal<T>
+    {
+        public BooleanVal(bool value, T obj)
+        {
+            Value = value;
+            Object = obj;
+        }
+
+        public bool Value { get; }
+
+        public T Object { get; }
+
+        public static implicit operator (bool Value, T Object)(BooleanVal<T> val)
+        {
+            return (val.Value, val.Object);
+        }
+
+        public static implicit operator BooleanVal<T>((bool Value, T Object) tuple)
+        {
+            return new(tuple.Value, tuple.Object);
+        }
+
+        public static implicit operator bool(BooleanVal<T> val)
+        {
+            return val.Value;
+        }
+
+        public static implicit operator BooleanVal<T>(bool value)
+        {
+            return new(value, default);
+        }
+    }
+
+    public static class BooleanValExtensions
+    {
+        public static BooleanVal<T> ToBooleanVal<T>(this bool value) => value;
+
+        public static BooleanVal<T> ToBooleanVal<T>(this bool value, T packagedValue) => new(value, packagedValue);
+
+        public static BooleanVal<T> ToBooleanVal<T>(this (bool, T) tuple) => tuple;
+    }
+}
