3e9fb7bf416abfae81e4d5463f1d1b1e3ed99097
alexinea <forerunner@outlook.com>
2021-02-08 16:50:14 +0800

    Add DateJudge

---
+++ src/Cosmos.Extensions.DateTime/Cosmos/Date/DateJudge.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Cosmos.Date
+{
+    public static class DateJudge
+    {
+        #region IsToday
+
+        /// <summary>
+        /// To judge whether the day is today.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static bool IsToday(DateTime dt) => dt.IsToday();
+
+        /// <summary>
+        /// To judge whether the day is today.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static bool IsToday(DateTime? dt) => dt.IsToday();
+
+        /// <summary>
+        /// To judge whether the day is today.
+        /// </summary>
+        /// <param name="dtOffset"></param>
+        /// <returns></returns>
+        public static bool IsToday(DateTimeOffset dtOffset) => dtOffset.IsToday();
+
+        /// <summary>
+        /// To judge whether the day is today.
+        /// </summary>
+        /// <param name="dtOffset"></param>
+        /// <returns></returns>
+        public static bool IsToday(DateTimeOffset? dtOffset) => dtOffset.IsToday();
+
+        #endregion
+
+        #region IsWeekend
+
+        /// <summary>
+        /// To judge whether the day is weekend.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static bool IsWeekend(DateTime dt) => dt.IsWeekend();
+
+        /// <summary>
+        /// To judge whether the day is weekend.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static bool IsWeekend(DateTime? dt) => dt.IsWeekend();
+
+        #endregion
+
+        #region IsWeekday
+
+        /// <summary>
+        /// To judge whether the day is weekday.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static bool IsWeekday(DateTime dt) => dt.IsWeekday();
+
+        /// <summary>
+        /// To judge whether the day is weekday.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static bool IsWeekday(DateTime? dt) => dt.IsWeekday();
+
+        #endregion
+    }
+}
