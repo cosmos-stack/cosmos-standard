b8cb21f49c3835aad1e2682ba838b62363c5fcf6
alexinea <forerunner@outlook.com>
2021-02-10 09:39:00 +0800

    Fix the bug about type judgment in Validation annotation.

--- src/Cosmos.Extensions.Validation.Basic/Cosmos/Validation/Annotations/Internals/ParameterValueGetter.cs
+++ src/Cosmos.Extensions.Validation.Basic/Cosmos/Validation/Annotations/Internals/ParameterValueGetter.cs
@@ -1,17 +1,36 @@
+using AspectCore.DynamicProxy.Parameters;
+
 namespace Cosmos.Validation.Annotations.Internals
 {
     internal static class ParameterValueGetter
     {
-        public static TValue TryTo<TValue>(this object obj)
+        public static TValue TryTo<TValue>(this Parameter parameter)
         {
-            return obj.TryTo(default(TValue));
+            return parameter.TryTo(default(TValue));
         }
 
-        public static TValue TryTo<TValue>(this object obj, TValue defaultValue)
+        public static TValue TryTo<TValue>(this decimal numericValue)
+        {
+            return numericValue.TryTo(default(TValue));
+        }
+
+        public static TValue TryTo<TValue>(this Parameter parameter, TValue defaultValue)
         {
             try
             {
-                return (TValue) obj;
+                return parameter.Value.As<TValue>();
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+        
+        public static TValue TryTo<TValue>(this decimal numericValue, TValue defaultValue)
+        {
+            try
+            {
+                return numericValue.As<TValue>();
             }
             catch
             {
