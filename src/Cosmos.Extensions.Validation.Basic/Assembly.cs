12f6f8f8c0d91e5bcf018e1409f2cf7ecb6120f8
alexinea <forerunner@outlook.com>
2021-03-27 15:09:03 +0800

    Merged validation annotations into Cosmos.Validation.

---
+++ src/Cosmos.Extensions.Validation.Basic/Assembly.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+//Cosmos Validation
+[assembly: InternalsVisibleTo("Cosmos.Validation")]
+[assembly: InternalsVisibleTo("Cosmos.Validation.Abstractions")]
+[assembly: InternalsVisibleTo("Cosmos.Validation.Annotations")]
