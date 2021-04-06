641cf4e78b892295810194e6062c2b2d4258211f
alexinea <forerunner@outlook.com>
2021-04-06 10:40:47 +0800

    Add CharGuard and CharJudge.

---
+++ src/Cosmos.Extensions.Validation.Basic/Cosmos/Text/CharJudge.cs
@@ -0,0 +1,14 @@
+﻿namespace Cosmos.Text
+{
+    public static class CharJudge
+    {
+        /// <summary>
+        /// To judge whether the char value is between left and right.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool IsBetween(char value, char left, char right) => value >= left && value <= right;
+    }
+}
---
+++ src/Cosmos/Cosmos/Text/CharMayOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Cosmos.Text
+{
+    public enum CharMayOptions
+    {
+        Default = 0,
+        IgnoreNullable = 1
+    }
+}
