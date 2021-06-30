9ce18b8ec974edff8dbb0c656993f14a6df65bb6
alexinea <forerunner@outlook.com>
2021-06-30 15:47:47 +0800

    Add IndexedTypeConverter interface.

--- src/Cosmos.Abstractions/Cosmos/ITypeConverter.cs
+++ src/Cosmos.Abstractions/Cosmos/ITypeConverter.cs
@@ -16,4 +16,22 @@
         /// <returns>类型转换的结果</returns>
         TTo To(TFrom from);
     }
+
+    /// <summary>
+    /// Type Converter Meta Interface with index<br />
+    /// 带索引值的类型转换元接口
+    /// </summary>
+    /// <typeparam name="TFrom">自何类型开始转换</typeparam>
+    /// <typeparam name="TTo">转换至何类型</typeparam>
+    public interface IIndexedTypeConverter<in TFrom, out TTo>
+    {
+        /// <summary>
+        /// To<br />
+        /// 类型转换至……
+        /// </summary>
+        /// <param name="from">被转换的对象</param>
+        /// <param name="index">索引值</param>
+        /// <returns>类型转换的结果</returns>
+        TTo To(TFrom from, int index);
+    }
 }
