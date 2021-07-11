ba4471f351b8af7a97a0180b96c9cd7f854890fc
alexinea <forerunner@outlook.com>
2021-07-11 11:29:59 +0800

    Update expression extensions.

--- src/Cosmos/Cosmos/Expressions/ExpressionExtensions.cs
+++ src/Cosmos/Cosmos/Expressions/ExpressionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using Cosmos.Reflection;
 
 namespace Cosmos.Expressions
 {
@@ -41,7 +43,7 @@ namespace Cosmos.Expressions
             if (parameterExpression is null)
                 throw new ArgumentNullException(nameof(parameterExpression));
 
-            if (!propertyInfo.DeclaringType.GetTypeInfo().IsAssignableFrom(parameterExpression.Type.GetTypeInfo()))
+            if (!propertyInfo.DeclaringType!.GetTypeInfo().IsAssignableFrom(parameterExpression.Type.GetTypeInfo()))
                 throw new InvalidOperationException(
                     $"The type of {nameof(parameterExpression)} ({parameterExpression.Type} " +
                     $"is not assignable to the type of {nameof(propertyInfo.DeclaringType)} " +
@@ -88,6 +90,75 @@ namespace Cosmos.Expressions
         }
 
         #endregion
-        
+
+        #region And/Or
+
+        // https://stackoverflow.com/questions/457316/combining-two-expressions-expressionfunct-bool/457328#457328
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+
+            var leftVisitor = new ReplaceExpressionVisitor(expr1.Parameters[0], parameter);
+            var left = leftVisitor.Visit(expr1.Body);
+            var rightVisitor = new ReplaceExpressionVisitor(expr2.Parameters[0], parameter);
+            var right = rightVisitor.Visit(expr2.Body);
+
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.OrElse(left!, right!), parameter);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+
+            var leftVisitor = new ReplaceExpressionVisitor(expr1.Parameters[0], parameter);
+            var left = leftVisitor.Visit(expr1.Body);
+            var rightVisitor = new ReplaceExpressionVisitor(expr2.Parameters[0], parameter);
+            var right = rightVisitor.Visit(expr2.Body);
+
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.AndAlso(left!, right!), parameter);
+        }
+
+        public static Expression<Func<T, bool>> AndIf<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2, bool condition)
+        {
+            if (!condition)
+            {
+                return expr1;
+            }
+
+            var parameter = Expression.Parameter(typeof(T));
+
+            var leftVisitor = new ReplaceExpressionVisitor(expr1.Parameters[0], parameter);
+            var left = leftVisitor.Visit(expr1.Body);
+            var rightVisitor = new ReplaceExpressionVisitor(expr2.Parameters[0], parameter);
+            var right = rightVisitor.Visit(expr2.Body);
+
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.AndAlso(left!, right!), parameter);
+        }
+
+        private sealed class ReplaceExpressionVisitor : ExpressionVisitor
+        {
+            private readonly Expression _oldValue;
+            private readonly Expression _newValue;
+
+            public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
+            {
+                _oldValue = oldValue;
+                _newValue = newValue;
+            }
+
+            public override Expression Visit(Expression node)
+            {
+                if (node == _oldValue)
+                    return _newValue;
+
+                return base.Visit(node)!;
+            }
+        }
+
+        #endregion
     }
 }
