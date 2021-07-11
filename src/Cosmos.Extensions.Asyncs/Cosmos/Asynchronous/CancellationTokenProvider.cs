1cd094e2edc266a6ebda668ff3026275256cdb7c
alexinea <forerunner@outlook.com>
2021-07-11 11:30:22 +0800

    Update async utilities.

---
+++ src/Cosmos.Extensions.Asyncs/Cosmos/Asynchronous/CancellationTokenProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+
+namespace Cosmos.Asynchronous
+{
+    public interface ICancellationTokenProvider
+    {
+        CancellationToken GetCancellationToken();
+    }
+
+    public sealed class NullCancellationTokenProvider : ICancellationTokenProvider
+    {
+        public CancellationToken GetCancellationToken() => default;
+    }
+}
---
+++ src/Cosmos.Extensions.Asyncs/Cosmos/Threading/AsyncLock.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cosmos.Threading
+{
+    /// <summary>
+    /// AsyncLock
+    /// </summary>
+    public sealed class AsyncLock : IDisposable
+    {
+        private readonly SemaphoreSlim _mutex = new(1, 1);
+
+        public IDisposable Lock()
+        {
+            _mutex.Wait();
+            return new AsyncLockReleaser(_mutex);
+        }
+
+        public Task<IDisposable> LockAsync()
+        {
+            return LockAsync(CancellationToken.None);
+        }
+
+        public Task<IDisposable> LockAsync(CancellationToken cancellationToken)
+        {
+            return LockAsync(TimeSpan.Zero, cancellationToken);
+        }
+
+        public async Task<IDisposable> LockAsync(TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            if (timeout <= TimeSpan.Zero)
+            {
+                await _mutex.WaitAsync(cancellationToken);
+            }
+            else
+            {
+                await _mutex.WaitAsync(timeout, cancellationToken);
+            }
+
+            return new AsyncLockReleaser(_mutex);
+        }
+
+        public void Dispose()
+        {
+            _mutex.Dispose();
+        }
+
+        private struct AsyncLockReleaser : IDisposable
+        {
+            private readonly SemaphoreSlim _semaphoreSlim;
+
+            public AsyncLockReleaser(SemaphoreSlim semaphoreSlim)
+            {
+                _semaphoreSlim = semaphoreSlim;
+            }
+
+            public void Dispose()
+            {
+                _semaphoreSlim.Release();
+            }
+        }
+    }
+}
