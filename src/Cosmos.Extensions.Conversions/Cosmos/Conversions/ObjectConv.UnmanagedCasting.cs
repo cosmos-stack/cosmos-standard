6112765c83e5b844e2b45679b49c868929b8bf94
alexinea <forerunner@outlook.com>
2022-10-05 21:47:54 +0800

    Update Conv

---
+++ src/Cosmos.Extensions.Conversions/Cosmos/Conversions/ObjectConv.UnmanagedCasting.cs
@@ -0,0 +1,14 @@
+﻿namespace Cosmos.Conversions;
+
+public static class UnmanagedCastingExtensions
+{
+    public static unsafe byte[] CastToBytes<TUnMan>(this TUnMan unmanagedObj) where TUnMan : unmanaged
+    {
+        var size = sizeof(TUnMan);
+        var result = new byte[size];
+        var p = (byte*)&unmanagedObj;
+        for (var i = 0; i < size; i++)
+            result[i] = *p++;
+        return result;
+    }
+}
